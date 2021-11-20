7782abe49b2a7337b3aac9e79edad535b9b490a7
doodspav <doodspav@gmail.com>
2021-11-20 01:55:30 +0000

    Add explicit support to tsx funcs
    [skip ci]

--- src/impl/tsx/abi/types.h
+++ src/impl/tsx/abi/types.h
@@ -14,6 +14,7 @@ typedef struct {
     patomic_func_view_t test_rtm;
     patomic_func_view_t cas_vptr;
     patomic_func_view_t cmpxchg_weak;
+    patomic_func_view_t explicit_cmpxchg_weak;
     size_t magic_size;
 } patomic_tsx_funcs_t;
 
@@ -25,10 +26,17 @@ typedef void (* patomic_tsx_cas_vptr_t) \
      void **expected,                   \
      const void *desired);
 
-typedef int (* patomic_tsx_cmpxchg_weak_t) \
-    (volatile void *obj,                   \
-     void *expected,                       \
+typedef int (* patomic_tsx_cmpxchg_t) \
+    (volatile void *obj,              \
+     void *expected,                  \
      const void *desired);
+
+typedef int (* patomic_tsx_explicit_cmpxchg_t) \
+    (volatile void *obj,                       \
+     void *expected,                           \
+     const void *desired,                      \
+     int,                                      \
+     int);
 
 static patomic_tsx_funcs_t
 patomic_tsx_get_funcs(void);
