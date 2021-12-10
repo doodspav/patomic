0204b92169353ef75180c387171a22c4d9880ddf
doodspav <doodspav@gmail.com>
2021-12-10 11:39:44 +0000

    Optimise alignment checks
    - align.recommended and align.minimum are both guaranteed to be a power of 2, so we can replace divmod with a bitwise version
    - align.size_within isn't guaranteed to be a power of 2, but it's fairly likely, so it's faster to put the divmod behind a conditional, and do the bitwise version if it is a power of 2
    
    [skip ci]

--- src/patomic/types/align.c
+++ src/patomic/types/align.c
@@ -8,6 +8,18 @@ patomic_cache_line_size(void)
     return (size_t) PATOMIC_MAX_CACHE_LINE_SIZE;
 }
 
+
+/* checks if x is a power of 2 */
+#define PATOMIC_IS_POW2(x) (((x) != 0) && (((x) & ((x) - 1u)) == 0))
+
+/* (x % y) assuming y is a power of 2 */
+#define PATOMIC_MOD_POW2(x, y) ((x) & ((y) - 1u))
+
+/* (x % y) with a conditionally fast case if y is a power of 2 */
+#define PATOMIC_MOD_CPOW2(x, y) \
+    (PATOMIC_IS_POW2(y) ? PATOMIC_MOD_POW2(x, y) : ((x) % (y)))
+
+
 int
 patomic_align_meets_recommended(
     const volatile void *ptr,
@@ -15,7 +27,7 @@ patomic_align_meets_recommended(
 )
 {
     patomic_uintptr_t addr = (patomic_uintptr_t) ptr;
-    return (addr % align.recommended == 0);
+    return (PATOMIC_MOD_POW2(addr, align.recommended) == 0);
 }
 
 int
@@ -26,9 +38,10 @@ patomic_align_meets_minimum(
 )
 {
     patomic_uintptr_t addr = (patomic_uintptr_t) ptr;
-    if (addr % align.minimum == 0)
+    if (PATOMIC_MOD_POW2(addr, align.minimum) == 0)
     {
-        addr %= align.size_within;
+        if (align.size_within == 0) { return 1; }
+        addr = (patomic_uintptr_t) PATOMIC_MOD_CPOW2(addr, align.size_within);
         return (addr + width) <= align.size_within;
     }
     else { return 0; }
