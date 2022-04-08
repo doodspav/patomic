0af4d83acbf066cddc30995ffa6910761464e4a0
doodspav <doodspav@gmail.com>
2022-04-08 18:24:21 +0100

    Add assertion in `patomic_get_kind`
    - assert that `id` has no more than a single bit set
    
    [skip ci]

--- src/patomic/types/ids.c
+++ src/patomic/types/ids.c
@@ -1,6 +1,18 @@
 #include <patomic/types/ids.h>
 
+#include <patomic/macros/force_inline.h>
+#include <patomic/stdlib/assert.h>
+
 #include "../../impl/register.h"
+
+
+PATOMIC_FORCE_INLINE static int
+patomic_is_pow2_or_zero(
+        unsigned int val
+)
+{
+    return (val & (val - 1)) == 0;
+}
 
 
 unsigned int
@@ -29,6 +41,8 @@ patomic_get_kind(
 {
     patomic_impl_t const *begin = patomic_impl_register;
     patomic_impl_t const *const end = begin + PATOMIC_IMPL_REGISTER_SIZE;
+
+    patomic_assert_always(patomic_is_pow2_or_zero((unsigned int) id));
 
     for (; begin != end; ++begin) {
         if (begin->id == id) { return begin->kind; }
