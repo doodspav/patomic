e15fe7ec3237bdc0873d71fa9bfcb2f37a50dfbc
doodspav <doodspav@gmail.com>
2023-10-21 18:12:36 +0100

    GHI #18 Fix and polish CMake (#19)
    The few visible changes made:
    - renamed `create_test(...)` to `_create_test(...)`
    - added `create_bt(...)` and `create_ut(...)`
    - BTs are skipped if `patomic::patomic` isn't available and can't be
    found
    - UTs are skipped if `patomic_test` isn't build as a sub-project of
    `patomic` (because `patomic` sets `PATOMIC_{BINARY, SOURCE}_DIR`
    variables for the UTs to find the required files)
    - `gmock` currently not linked because it breaks automatic test lookup
    (might add later once there's a pipeline)
    
    ---------
    
    Signed-off-by: doodspav <doodspav@gmail.com>

--- test/cmake/OptionVariables.cmake
+++ test/cmake/OptionVariables.cmake
@@ -1,28 +1,46 @@
+# ---- Options Summary ----
+
+# --------------------------------------------------------------------
+# | Option                               | Availability | Default    |
+# |======================================|==============|============|
+# | CMAKE_INSTALL_TESTDIR (unofficial)   | Always       | share/test |
+# |--------------------------------------|--------------|------------|
+# | PATOMIC_CREATE_TEST_TARGETS_MATCHING | Always       | ^(.*)$     |
+# | PATOMIC_WINDOWS_SET_CTEST_PATH_ENV   | Always       | ON         |
+# | PATOMIC_WINDOWS_CREATE_PATH_ENV_FILE | Always       | OFF        |
+# --------------------------------------------------------------------
+
+
 # ---- Test Install Directory ----
 
 # Normally tests would be install in CMAKE_INSTALL_BINDIR by default since
 # they're executables.
 # This is undesirable, so this variable exists to override the install location
 # of test binaries separately.
-# It's not prefixed with patomic_test because it's ok for it to be shared and
+# It's not prefixed with PATOMIC_ because it's ok for it to be shared and
 # overridden by parent projects.
 # Note: this is not an official CMake variable
+# The variable type is STRING rather than PATH, because otherwise passing
+# -DCMAKE_INSTALL_TESTDIR=share/test on the command line would expand to an
+# absolute path with the base being the current CMake directory, leading to
+# unexpected errors.
 set(
     CMAKE_INSTALL_TESTDIR "share/test"
-    CACHE PATH "(unofficial) Default test install location"
+    CACHE STRING "(unofficial) Default test install location"
 )
 
 
 # ---- Test Build Selection ----
 
-# This option provides a way to selectively disable/enable tests based on target
-# name (not suite/case name, or name passed to create_test).
+# This option provides a way to selectively disable/enable building tests based
+# on target name (not suite/case name, or name passed to create_test).
 # Regex must be written according to CMake Regex Specification.
 # By default all test targets are enabled.
 set(
     PATOMIC_CREATE_TEST_TARGETS_MATCHING "^(.*)$"
     CACHE STRING "Only test targets matching regex are created and registered with CTest"
 )
+mark_as_advanced(PATOMIC_CREATE_TEST_TARGETS_MATCHING)
 
 
 # ---- Windows Tests Path ----
@@ -30,8 +48,9 @@ set(
 # By default we set PATH for tests run with CTest on Windows in order to prevent
 # linker errors.
 # Due to limitations in CMake, we can only completely override the PATH, rather
-# than prepend to it.
+# than prepend or append to it.
 # This gives users the option to disable this behaviour.
+# This option has no effect when not running on Windows.
 option(
     PATOMIC_WINDOWS_SET_CTEST_PATH_ENV
     "Set PATH environment variable for tests when run CTest on Windows"
@@ -42,13 +61,16 @@ mark_as_advanced(PATOMIC_WINDOWS_SET_CTEST_PATH_ENV)
 
 # ---- Windows Path File ----
 
-# By default on Windows we generate a file per test kind that contains a string
-# that can be prepended to PATH before running tests, in order to ensure that
+# On Windows we need to set PATH for tests but may not want to have the PATH be
+# completely overridden, like with PATOMIC_WINDOWS_SET_CTEST_PATH_ENV.
+# Instead we can generated a file per test kind that contains a string that can
+# be manually prepended to PATH before running tests, in order to ensure that
 # runtime dependencies can be found.
 # Most of the time we don't need this file (since CTest will take care of that
-# for us), so we don't need to generate it.
+# for us), so we don't generate it by default.
 # Additionally disabled by default because it contains potentially private
 # information about the target platform.
+# This option has no effect when not running on Windows.
 option(
     PATOMIC_WINDOWS_CREATE_PATH_ENV_FILE
     "Create file with PATH environment variables for tests on Windows"
