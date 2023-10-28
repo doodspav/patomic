5582444d5052e6c32047c866df96a0903b5f5886
doodspav <doodspav@gmail.com>
2023-10-28 23:02:08 +0100

    GHI #20 Add ci tests for asan

--- test/ci/check_asan.cpp
+++ test/ci/check_asan.cpp
@@ -1,7 +1,46 @@
 #include <gtest/gtest.h>
+#include <gtest/gtest-spi.h>
 
 
-TEST(CiSuite, ASan)
+class CiAsan : public testing::Test
+{};
+
+
+TEST_F(CiAsan, UseAfterFree)
 {
-    ASSERT_TRUE(true);
+    int *p = new int(5);
+    volatile int _ = *p;
+    delete p;
+
+    volatile int val;
+    EXPECT_NONFATAL_FAILURE(val = *p, "asan");
+}
+
+TEST_F(CiAsan, HeapBufferOverflow)
+{
+    int *arr = new int[100]{};
+    int index = 1;
+
+    volatile int val;
+    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
+
+    delete[] arr;
+}
+
+TEST_F(CiAsan, StackBufferOverflow)
+{
+    int arr[100]{};
+    int index = 1;
+
+    volatile int val;
+    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
+}
+
+TEST_F(CiAsan, StaticBufferOverflow)
+{
+    static int arr[100]{};
+    int index = 1;
+
+    volatile int val;
+    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
 }
--- test/src/gtest_sanitizer_support.cpp
+++ test/src/gtest_sanitizer_support.cpp
@@ -1,21 +1,22 @@
 #include <gtest/gtest.h>
 
+
 extern "C" {
     [[maybe_unused]] void
     __asan_on_error()
     {
-        GTEST_FAIL() << "Encountered an address sanitizer error";
+        GTEST_FAIL() << "Encountered an address sanitizer (asan) error";
     }
 
     [[maybe_unused]] inline void
     __tsan_on_report()
     {
-        GTEST_FAIL() << "Encountered a thread sanitizer error";
+        GTEST_FAIL() << "Encountered a thread sanitizer (tsan) error";
     }
 
     [[maybe_unused]] inline void
     __ubsan_on_report()
     {
-        GTEST_FAIL() << "Encountered an undefined behaviour sanitizer error";
+        GTEST_FAIL() << "Encountered an undefined behaviour sanitizer (ubsan) error";
     }
 }
