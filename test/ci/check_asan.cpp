4edbd9ee1d7b553c0f82b8e189de17baf3709865
doodspav <doodspav@gmail.com>
2023-10-29 02:06:48 +0000

    GHI #20 Use death tests in CiAsan test suite

--- test/ci/check_asan.cpp
+++ test/ci/check_asan.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <gtest/gtest-spi.h>
 
 
 class CiAsan : public testing::Test
@@ -8,39 +7,47 @@ class CiAsan : public testing::Test
 
 TEST_F(CiAsan, UseAfterFree)
 {
-    int *p = new int(5);
-    volatile int _ = *p;
-    delete p;
+    auto fn = []() noexcept -> void {
+        int *p = new int(5);
+        volatile int val = *p;
+        delete p;
 
-    volatile int val;
-    EXPECT_NONFATAL_FAILURE(val = *p, "asan");
+        volatile int _ = *p;  // <-- use after free (intentional)
+    };
+    EXPECT_DEATH(fn(), ".*AddressSanitizer: heap-use-after-free.*");
 }
 
 TEST_F(CiAsan, HeapBufferOverflow)
 {
-    int *arr = new int[100]{};
-    int index = 1;
+    auto fn = []() noexcept -> void {
+        int *arr = new int[100]{};
+        int i = 1;
 
-    volatile int val;
-    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
+        volatile int _ = arr[100 + i];  // <-- buffer overflow (intentional)
 
-    delete[] arr;
+        delete[] arr;
+    };
+    EXPECT_DEATH(fn(), ".*AddressSanitizer: heap-buffer-overflow.*");
 }
 
 TEST_F(CiAsan, StackBufferOverflow)
 {
-    int arr[100]{};
-    int index = 1;
+    auto fn = []() noexcept -> void {
+        int arr[100]{};
+        int i = 1;
 
-    volatile int val;
-    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
+        volatile int _ = arr[100 + i];  // <-- buffer overflow (intentional)
+    };
+    EXPECT_DEATH(fn(), ".*AddressSanitizer: stack-buffer-overflow.*");
 }
 
-TEST_F(CiAsan, StaticBufferOverflow)
+TEST_F(CiAsan, GlobalBufferOverflow)
 {
-    static int arr[100]{};
-    int index = 1;
+    auto fn = []() noexcept -> void {
+        static int arr[100]{};
+        int i = 1;
 
-    volatile int val;
-    EXPECT_NONFATAL_FAILURE(val = arr[100 + index], "asan");
+        volatile int _ = arr[100 + i];  // <-- buffer overflow (intentional)
+    };
+    EXPECT_DEATH(fn(), ".*AddressSanitizer: global-buffer-overflow.*");
 }
