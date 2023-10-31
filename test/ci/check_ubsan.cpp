c0cdb930466303acef68391545d9d57e57530472
doodspav <doodspav@gmail.com>
2023-10-31 02:14:49 +0000

    GHI #20 Try something
    Signed-off-by: doodspav <doodspav@gmail.com>

--- test/ci/check_ubsan.cpp
+++ test/ci/check_ubsan.cpp
@@ -4,6 +4,34 @@
 #include <climits>
 #include <limits>
 
+extern "C" {
+
+/// @brief Called by asan when an error is detected.
+/// @note  Asan may still abort after calling this function.
+void
+__asan_on_error()
+{
+    GTEST_FAIL() << "Encountered an address sanitizer (asan) error";
+}
+
+/// @brief Called by tsan when an error is detected.
+/// @note  Tsan may still abort after calling this function.
+void
+__tsan_on_report()
+{
+    GTEST_FAIL() << "Encountered a thread sanitizer (tsan) error";
+}
+
+/// @brief Called by ubsan when an error is detected .
+/// @note  Ubsan may still abort after calling this function.
+void
+__ubsan_on_report()
+{
+    GTEST_FAIL() << "Encountered an undefined behaviour sanitizer (ubsan) error";
+}
+
+}  // extern "C"
+
 
 class CiUbsanDeathTest : public testing::Test
 {};
