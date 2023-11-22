69c7f6edfdc8cd6f17e8004755bb1fdcff990219
doodspav <doodspav@gmail.com>
2023-11-23 00:59:18 +0200

    GHI #20 Use environment variables in toolchain file [skip-ci]

--- ci/toolchain-ubuntu-gcc-cross.cmake
+++ ci/toolchain-ubuntu-gcc-cross.cmake
@@ -1,14 +1,19 @@
-# variable that might come from outside
-set(processor aarch64)
-set(version 11)
+set(architecture "$ENV{PATOMIC_CI_XARCH}")
+set(version "$ENV{PATOMIC_CI_XCOMPILER_VERSION}")
+
+if(NOT architecture)
+    message(FATAL_ERROR "PATOMIC_CI_XARCH environment variable is not set")
+elseif(NOT version)
+    message(FATAL_ERROR "PATOMIC_CI_XCOMPILER_VERSION environment variable is not set")
+endif()
 
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR ${processor})
+set(CMAKE_SYSTEM_architecture ${architecture})
 
 set(CMAKE_SYSROOT "$ENV{HOME}/sysroot")
-set(CMAKE_C_COMPILER ${processor}-linux-gnu-gcc-${version})
-set(CMAKE_CXX_COMPILER ${processor}-linux-gnu-g++-${version})
-set(CMAKE_AR ${processor}-linux-gnu-ar)
+set(CMAKE_C_COMPILER ${architecture}-linux-gnu-gcc-${version})
+set(CMAKE_CXX_COMPILER ${architecture}-linux-gnu-g++-${version})
+set(CMAKE_AR ${architecture}-linux-gnu-ar)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
