9cf9090e4413bd84b2ee8ed5140b672b457331c9
doodspav <doodspav@gmail.com>
2023-11-23 04:36:36 +0200

    GHI #20 Get architecture from workflow file

--- ci/toolchain-qemu-ubuntu-cross.cmake
+++ ci/toolchain-qemu-ubuntu-cross.cmake
@@ -1,4 +1,5 @@
 # get environment variables set by CI
+set(architecture "$ENV{PATOMIC_CI_XARCH}")
 set(triple "$ENV{PATOMIC_CI_XTRIPLE}")
 set(compiler "$ENV{PATOMIC_CI_XCOMPILER}")
 set(version "$ENV{PATOMIC_CI_XCOMPILER_VERSION}")
@@ -18,10 +19,6 @@ set(version "$ENV{PATOMIC_CI_XCOMPILER_VERSION}")
 #     message(FATAL_ERROR "PATOMIC_CI_XCOMPILER_VERSION environment variable '${version}' is not a number")
 # endif()
 
-# get target architecture from triple
-# string(REGEX_MATCH "[a-zA-Z0-9]+" architecture "${triple}")
-set(architecture "aarch64")
-
 # set basic target information
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR "${architecture}")
@@ -30,22 +27,21 @@ set(CMAKE_SYSTEM_PROCESSOR "${architecture}")
 set(CMAKE_SYSROOT "$ENV{HOME}/sysroot")
 set(CMAKE_CROSSCOMPILING_EMULATOR "qemu-${architecture};-L;${CMAKE_SYSROOT}")
 
-# set the appropriate cross compilers and their target architecture triples
+# set the appropriate cross compilers and archiver
 # value of ${compiler} is checked at start when obtained from environment variable
 if(compiler MATCHES "clang")
     set(CMAKE_C_COMPILER "clang-${version}")
     set(CMAKE_CXX_COMPILER "clang++-${version}")
+    set(CMAKE_AR "llvm-ar-${version}")
 elseif(compiler MATCHES "gcc")
     set(CMAKE_C_COMPILER "${triple}-gcc-${version}")
     set(CMAKE_CXX_COMPILER "${triple}-g++-${version}")
+    set(CMAKE_AR "${triple}-ar")
 endif()
 
 # set the target triple matching the cross compilers above
 set(CMAKE_C_COMPILER_TARGET "${triple}")
 set(CMAKE_CXX_COMPILER_TARGET "${triple}")
-
-# use an archiver that works with the compilers above
-set(CMAKE_AR "llvm-ar-${version}")
 
 # search for programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
