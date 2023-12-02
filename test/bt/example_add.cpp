3f87732e99aec164c56b0ad98b4a6053373bc2f7
doodspav <doodspav@gmail.com>
2023-12-02 06:56:35 +0200

    GHI #20 Remove xml changes [skip-ci]

--- test/bt/example_add.cpp
+++ test/bt/example_add.cpp
@@ -7,6 +7,4 @@
 TEST(BtSuite, Add)
 {
     ASSERT_EQ(5, patomic_example_add(2, 3));
-    std::cout << "<sender>John</sender>" << std::endl;
-    std::cerr << "<sender>John</sender>" << std::endl;
 }
