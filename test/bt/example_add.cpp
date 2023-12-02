eb742f9615cfe0914204e8b58752ec24ef7f974d
doodspav <doodspav@gmail.com>
2023-12-02 06:37:25 +0200

    GHI #20 Try break junit xml

--- test/bt/example_add.cpp
+++ test/bt/example_add.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include <patomic/patomic.h>
 
+#include <iostream>
+
 
 TEST(BtSuite, Add)
 {
     ASSERT_EQ(5, patomic_example_add(2, 3));
+    std::cout << "<sender>John</sender>" << std::endl;
+    std::cerr << "<sender>John</sender>" << std::endl;
 }
