c77d82400bc80e12ba059d64e6d1b4d06319cc0c
doodspav <doodspav@gmail.com>
2024-06-18 00:07:20 +0100

    GHI #32 Remove uts

--- test/kind/ut/example_add.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <patomic/patomic.h>
-
-
-TEST(UtSuite, Add)
-{
-    ASSERT_EQ(5, 2 + 3);
-}
--- test/kind/ut/example_sub.cpp
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <patomic/patomic.h>
-
-
-TEST(UtSuite, Sub)
-{
-    ASSERT_EQ(5, 2 + 3);
-}
