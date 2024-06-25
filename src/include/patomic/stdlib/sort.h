252ff907a26f687831d40c5c4de5a040e282bd5d
doodspav <doodspav@gmail.com>
2024-06-25 01:05:57 -0400

    GHI #32 Abstract away qsort as patomic_sort

---
+++ src/include/patomic/stdlib/sort.h
@@ -0,0 +1,42 @@
+#ifndef PATOMIC_STDLIB_SORT_H
+#define PATOMIC_STDLIB_SORT_H
+
+#include <stdlib.h>
+
+
+/**
+ * @addtogroup stdlib
+ *
+ * @brief
+ *   Sorts the given array pointed to by "ptr" in ascending order. The array
+ *   contains "count" elements of "size" bytes. Function pointed to by "comp"
+ *   is used for object comparison.
+ *
+ * @param ptr
+ *   Pointer to the array to sort.
+ *
+ * @param count
+ *   Number of elements in the array.
+ *
+ * @param size
+ *   Size of each element in the array in bytes.
+ *
+ * @param comp
+ *   Comparison function which returns a negative integer value if the first
+ *   argument is less than the second, a positive integer value if the first
+ *   argument is greater than the second and zero if the arguments are
+ *   equivalent.
+ *
+ * @note
+ *   The interface is identical to C90's qsort.
+ */
+void
+patomic_sort(
+    void *ptr,
+    size_t count,
+    size_t size,
+    int (*comp)(const void*, const void*)
+);
+
+
+#endif  /* PATOMIC_STDLIB_SORT_H */
