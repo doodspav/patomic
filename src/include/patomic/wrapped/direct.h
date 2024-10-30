9d7e4472e59d3b6b87c6c473b4c3fe143c5c245d
doodspav <doodspav@gmail.com>
2024-10-30 04:24:53 +0000

    GHI #35 Add wrapped macro for direct fetch

--- src/include/patomic/wrapped/direct.h
+++ src/include/patomic/wrapped/direct.h
@@ -609,4 +609,108 @@
     }
 
 
+/**
+ * @addtogroup wrapped.direct
+ *
+ * @brief
+ *   Defines a function which implements an unary atomic fetch operation using
+ *   unary fetch as the underlying atomic operation.
+ *
+ * @details
+ *   The defined function's signature will match either patomic_opsig_fetch_t
+ *   or patomic_opsig_explicit_fetch_t (depending on the value of 'vis_p').
+ *
+ * @param atomic_type
+ *   The type of the object on which the atomic operation is to be performed.
+ *   Must not be a VLA or an array of unknown size.
+ *
+ * @param type
+ *   The non-atomic counterpart of 'atomic_type'. This must have the same size
+ *   as 'atomic_type' and must not have a stricter alignment.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param vis_p
+ *   Either the macro 'SHOW_P' if the function should be defined as taking a
+ *   memory order parameter (a.k.a. explicit), or the macro 'HIDE_P' if it
+ *   should not (a.k.a. implicit).
+ *
+ * @param order
+ *   The literal token 'order' if 'vis_p' is 'SHOW_P', otherwise the desired
+ *   memory order to be used implicitly by the atomic operation.
+ *
+ * @param do_atomic_fetch_explicit
+ *   A macro, M, callable as 'M(obj, arg, order, res) where:
+ *   - the result of the expression is unused
+ *   - 'obj' will be an expression of the type 'volatile atomic_type *'
+ *   - 'arg' will be the name of a local identifier, with the type 'type'
+ *   - 'order' will be an expression of type 'int' whose value is a valid
+ *     memory order
+ *   - 'res' will be the name of a local identifier, with the type 'type'
+ *
+ *   The expected behaviour of calling the macro M as above is:
+ *   - the value of the object pointed to by 'obj' is read and modified in
+ *     a single atomic operation
+ *   - 'res' is set to the original value read from the object pointed to by
+ *     'obj'
+ *   - the atomic operation uses a memory ordering at least as strong as
+ *     'order'
+ *
+ *   The following local variables will also be available to be used by the
+ *   macro M:
+ *   - 'temp' has type 'int'
+ *   - 'scratch' has type 'type'
+ *   - their value is unspecified and they may be uninitialized
+ */
+#define PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(                         \
+    atomic_type, type, fn_name, vis_p, order,                           \
+    do_atomic_fetch_explicit                                            \
+)                                                                       \
+    static void                                                         \
+    fn_name(                                                            \
+        volatile void *const obj                                        \
+        ,const void *const argument                                     \
+ vis_p(_,int order)                                                     \
+        ,void *const ret                                                \
+    )                                                                   \
+    {                                                                   \
+        /* static assertions */                                         \
+        PATOMIC_STATIC_ASSERT(                                          \
+            sizeof_type_eq_atype, sizeof(type) == sizeof(atomic_type)); \
+                                                                        \
+        /* declarations */                                              \
+        type arg;                                                       \
+        type res;                                                       \
+        type scratch;                                                   \
+        int temp;                                                       \
+                                                                        \
+        /* assertions */                                                \
+        PATOMIC_WRAPPED_DO_ASSERT(obj != NULL);                         \
+        PATOMIC_WRAPPED_DO_ASSERT(argument != NULL);                    \
+        PATOMIC_WRAPPED_DO_ASSERT(ret != NULL);                         \
+        PATOMIC_WRAPPED_DO_ASSERT_ALIGNED(obj, atomic_type);            \
+        PATOMIC_WRAPPED_DO_ASSERT(PATOMIC_IS_VALID_ORDER((int) order)); \
+                                                                        \
+        /* setup */                                                     \
+        PATOMIC_WRAPPED_DO_MEMCPY(&arg, argument, sizeof(type));        \
+                                                                        \
+        /* operation */                                                 \
+        do_atomic_fetch_explicit(                                       \
+            (volatile atomic_type *) obj,                               \
+            arg,                                                        \
+            (int) order,                                                \
+            res                                                         \
+        );                                                              \
+                                                                        \
+        /* cleanup */                                                   \
+        PATOMIC_WRAPPED_DO_MEMCPY(ret, &res, sizeof(type));             \
+        PATOMIC_IGNORE_UNUSED(scratch);                                 \
+        PATOMIC_IGNORE_UNUSED(temp);                                    \
+    }
+
+
+
+
+
 #endif  /* PATOMIC_WRAPPED_DIRECT_H */
