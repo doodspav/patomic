8246dce33612f046fd3aba82fd15b2dcbeee07b4
doodspav <doodspav@gmail.com>
2024-10-30 03:35:22 +0000

    GHI #35 Add wrapped macro for direct bit test/modify

--- src/include/patomic/wrapped/direct.h
+++ src/include/patomic/wrapped/direct.h
@@ -8,6 +8,7 @@
 
 #include <patomic/api/memory_order.h>
 
+#include <limits.h>
 #include <stddef.h>
 
 
@@ -56,8 +57,8 @@
  *   - the atomic operation uses a store memory ordering at least as strong as
  *     'order'
  *
- *   The following local variables will also be available to be used by
- *   the macro M:
+ *   The following local variables will also be available to be used by the
+ *   macro M:
  *   - 'temp' has type 'int'
  *   - 'scratch' has type 'type'
  *   - their value is unspecified and they may be uninitialized
@@ -68,9 +69,9 @@
 )                                                                             \
     static void                                                               \
     fn_name(                                                                  \
-        volatile void *obj                                                    \
-        ,const void *desired                                                  \
- vis_p(_,int order)                                                           \
+        volatile void *const obj                                              \
+        ,const void *const desired                                            \
+ vis_p(_,const int order)                                                     \
     )                                                                         \
     {                                                                         \
         /* static assertions */                                               \
@@ -149,8 +150,8 @@
  *   - the atomic operation uses a load memory ordering at least as strong as
  *     'order'
  *
- *   The following local variables will also be available to be used by
- *   the macro M:
+ *   The following local variables will also be available to be used by the
+ *   macro M:
  *   - 'temp' has type 'int'
  *   - 'scratch' has type 'type'
  *   - their value is unspecified and they may be uninitialized
@@ -161,9 +162,9 @@
 )                                                                            \
     static void                                                              \
     fn_name(                                                                 \
-        volatile void *obj                                                   \
- vis_p(_,int order)                                                          \
-        ,void *ret                                                           \
+        volatile void *const obj                                             \
+ vis_p(_,const int order)                                                    \
+        ,void *const ret                                                     \
     )                                                                        \
     {                                                                        \
         /* static assertions */                                              \
@@ -243,8 +244,8 @@
  *   - the atomic operation uses a memory ordering at least as strong as
  *     'order'
  *
- *   The following local variables will also be available to be used by
- *   the macro M:
+ *   The following local variables will also be available to be used by the
+ *   macro M:
  *   - 'temp' has type 'int'
  *   - 'scratch' has type 'type'
  *   - their value is unspecified and they may be uninitialized
@@ -255,10 +256,10 @@
 )                                                                       \
     static void                                                         \
     fn_name(                                                            \
-        volatile void *obj                                              \
-        ,const void *desired                                            \
- vis_p(_,int order)                                                     \
-        ,void *ret                                                      \
+        volatile void *const obj                                        \
+        ,const void *const desired                                      \
+ vis_p(_,const int order)                                               \
+        ,void *const ret                                                \
     )                                                                   \
     {                                                                   \
         /* static assertions */                                         \
@@ -355,8 +356,8 @@
  *     for a successful exchange, and a load memory ordering at least as strong
  *     as 'fail' for a failed exchange
  *
- *   The following local variables will also be available to be used by
- *   the macro M:
+ *   The following local variables will also be available to be used by the
+ *   macro M:
  *   - 'temp' has type 'int'
  *   - 'scratch' has type 'type'
  *   - their value is unspecified and they may be uninitialized
@@ -367,11 +368,11 @@
 )                                                                           \
     static int                                                              \
     fn_name(                                                                \
-        volatile void *obj                                                  \
-        ,void *expected                                                     \
-        ,const void *desired                                                \
- vis_p(_,int succ)                                                          \
- vis_p(_,int fail)                                                          \
+        volatile void *const obj                                            \
+        ,void *const expected                                               \
+        ,const void *const desired                                          \
+ vis_p(_,const int succ)                                                    \
+ vis_p(_,const int fail)                                                    \
     )                                                                       \
     {                                                                       \
         /* static assertions */                                             \
@@ -413,6 +414,198 @@
         PATOMIC_IGNORE_UNUSED(scratch);                                     \
         PATOMIC_IGNORE_UNUSED(temp);                                        \
         return ok != 0;                                                     \
+    }
+
+
+/**
+ * @addtogroup wrapped.direct
+ *
+ * @brief
+ *   Defines a function which implements an atomic bit test operation using
+ *   bit test as the underlying atomic operation.
+ *
+ * @details
+ *   The defined function's signature will match either patomic_opsig_test_t
+ *   or patomic_opsig_explicit_test_t (depending on the value of 'vis_p').
+ *
+ * @param atomic_type
+ *   The type of the object on which the atomic operation is to be performed.
+ *   Must not be a VLA or an array of unknown size.
+ *
+ * @param type
+ *   The non-atomic counterpart of 'atomic_type'. This must have the same size
+ *   as 'atomic_type' and must not have a stricter alignment.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param vis_p
+ *   Either the macro 'SHOW_P' if the function should be defined as taking a
+ *   memory order parameter (a.k.a. explicit), or the macro 'HIDE_P' if it
+ *   should not (a.k.a. implicit).
+ *
+ * @param order
+ *   The literal token 'order' if 'vis_p' is 'SHOW_P', otherwise the desired
+ *   memory order to be used implicitly by the atomic operation.
+ *
+ * @param do_atomic_bit_test_explicit
+ *   A macro, M, callable as 'M(obj, offset, order, res)' where:
+ *   - the result of the expression is unused
+ *   - 'obj' will be an expression of the type 'const volatile atomic_type *'
+ *   - 'offset' will be an expression of type 'int' whose value is non-negative
+ *     and less than 'sizeof(type) * CHAR_BIT'
+ *   - 'order' will be an expression of type 'int' whose value is a valid
+ *     load memory order
+ *   - 'res' will be the name of a local identifier, with the type 'int'
+ *
+ *   The expected behaviour of calling the macro M as above is:
+ *   - the value of the object pointed to by 'obj' is atomically read
+ *   - 'res' is set to the bit at offset 'offset' of the value which was read
+ *   - the atomic operation uses a load memory ordering at least as strong as
+ *     'order'
+ *
+ *   The following local variables will also be available to be used by the
+ *   macro M:
+ *   - 'temp' has type 'int'
+ *   - 'scratch' has type 'type'
+ *   - their value is unspecified and they may be uninitialized
+ */
+#define PATOMIC_WRAPPED_DIRECT_DEFINE_OP_BIT_TEST(                              \
+    atomic_type, type, fn_name, vis_p, order,                                   \
+    do_atomic_bit_test_explicit                                                 \
+)                                                                               \
+    static int                                                                  \
+    fn_name(                                                                    \
+        const volatile void *const obj                                          \
+        ,const int offset                                                       \
+ vis_p(_,const int order)                                                       \
+    )                                                                           \
+    {                                                                           \
+        /* static assertions */                                                 \
+        PATOMIC_STATIC_ASSERT(                                                  \
+            sizeof_type_eq_atype, sizeof(type) == sizeof(atomic_type));         \
+                                                                                \
+        /* declarations */                                                      \
+        type scratch;                                                           \
+        int temp;                                                               \
+        int res;                                                                \
+                                                                                \
+        /* assertions */                                                        \
+        PATOMIC_WRAPPED_DO_ASSERT(obj != NULL);                                 \
+        PATOMIC_WRAPPED_DO_ASSERT_ALIGNED(obj, atomic_type);                    \
+        PATOMIC_WRAPPED_DO_ASSERT(offset >= 0);                                 \
+        PATOMIC_WRAPPED_DO_ASSERT((size_t) offset < (sizeof(type) * CHAR_BIT)); \
+        PATOMIC_WRAPPED_DO_ASSERT(PATOMIC_IS_VALID_LOAD_ORDER((int) order));    \
+                                                                                \
+        /* operation */                                                         \
+        do_atomic_bit_test_explicit(                                            \
+            (const volatile atomic_type *) obj,                                 \
+            offset,                                                             \
+            (int) order,                                                        \
+            res                                                                 \
+        );                                                                      \
+                                                                                \
+        /* cleanup */                                                           \
+        PATOMIC_IGNORE_UNUSED(scratch);                                         \
+        PATOMIC_IGNORE_UNUSED(temp);                                            \
+        return res != 0;                                                        \
+    }
+
+
+/**
+ * @addtogroup wrapped.direct
+ *
+ * @brief
+ *   Defines a function which implements an atomic bit test-modify operation
+ *   using bit test-modify as the underlying atomic operation.
+ *
+ * @details
+ *   The defined function's signature will match either
+ *   patomic_opsig_test_modify_t or patomic_opsig_explicit_test_modify_t
+ *   (depending on the value of 'vis_p').
+ *
+ * @param atomic_type
+ *   The type of the object on which the atomic operation is to be performed.
+ *   Must not be a VLA or an array of unknown size.
+ *
+ * @param type
+ *   The non-atomic counterpart of 'atomic_type'. This must have the same size
+ *   as 'atomic_type' and must not have a stricter alignment.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param vis_p
+ *   Either the macro 'SHOW_P' if the function should be defined as taking a
+ *   memory order parameter (a.k.a. explicit), or the macro 'HIDE_P' if it
+ *   should not (a.k.a. implicit).
+ *
+ * @param order
+ *   The literal token 'order' if 'vis_p' is 'SHOW_P', otherwise the desired
+ *   memory order to be used implicitly by the atomic operation.
+ *
+ * @param do_atomic_bit_test_modify_explicit
+ *   A macro, M, callable as 'M(obj, offset, order, res)' where:
+ *   - the result of the expression is unused
+ *   - 'obj' will be an expression of the type 'volatile atomic_type *'
+ *   - 'offset' will be an expression of type 'int' whose value is non-negative
+ *     and less than 'sizeof(type) * CHAR_BIT'
+ *   - 'order' will be an expression of type 'int' whose value is a valid
+ *     memory order
+ *   - 'res' will be the name of a local identifier, with the type 'int'
+ *
+ *   The expected behaviour of calling the macro M as above is:
+ *   - the bit at offset 'offset' in the object pointed to by 'obj' is modified
+ *     in a single atomic operation
+ *   - the atomic operation uses a store memory ordering at least as strong as
+ *     'order' if the operation does not require reading the old value,
+ *     otherwise it uses a memory ordering at least as strong as 'order'
+ *
+ *   The following local variables will also be available to be used by the
+ *   macro M:
+ *   - 'temp' has type 'int'
+ *   - 'scratch' has type 'type'
+ *   - their value is unspecified and they may be uninitialized
+ */
+#define PATOMIC_WRAPPED_DIRECT_DEFINE_OP_BIT_TEST_MODIFY(                       \
+    atomic_type, type, fn_name, vis_p, order,                                   \
+    do_atomic_bit_test_modify_explicit                                          \
+)                                                                               \
+    static int                                                                  \
+    fn_name(                                                                    \
+        volatile void *const obj                                                \
+        ,const int offset                                                       \
+ vis_p(_,const int order)                                                       \
+    )                                                                           \
+    {                                                                           \
+        /* static assertions */                                                 \
+        PATOMIC_STATIC_ASSERT(                                                  \
+            sizeof_type_eq_atype, sizeof(type) == sizeof(atomic_type));         \
+                                                                                \
+        /* declarations */                                                      \
+        type scratch;                                                           \
+        int temp;                                                               \
+        int res;                                                                \
+                                                                                \
+        /* assertions */                                                        \
+        PATOMIC_WRAPPED_DO_ASSERT(obj != NULL);                                 \
+        PATOMIC_WRAPPED_DO_ASSERT_ALIGNED(obj, atomic_type);                    \
+        PATOMIC_WRAPPED_DO_ASSERT(offset >= 0);                                 \
+        PATOMIC_WRAPPED_DO_ASSERT((size_t) offset < (sizeof(type) * CHAR_BIT)); \
+        PATOMIC_WRAPPED_DO_ASSERT(PATOMIC_IS_VALID_ORDER((int) order));         \
+                                                                                \
+        /* operation */                                                         \
+        do_atomic_bit_test_modify_explicit(                                     \
+            (volatile atomic_type *) obj,                                       \
+            offset,                                                             \
+            (int) order,                                                        \
+            res                                                                 \
+        );                                                                      \
+                                                                                \
+        /* cleanup */                                                           \
+        PATOMIC_IGNORE_UNUSED(scratch);                                         \
+        PATOMIC_IGNORE_UNUSED(temp);                                            \
+        return res != 0;                                                        \
     }
 
 
