80a7eb6646f9dce06ea79f51c13f594e995b40c2
doodspav <doodspav@gmail.com>
2024-10-31 00:52:30 +0000

    GHI #35 Add cmpxchg_strong to cmpxchg wrapped

--- src/include/patomic/wrapped/cmpxchg.h
+++ src/include/patomic/wrapped/cmpxchg.h
@@ -223,4 +223,157 @@
     }
 
 
+/**
+ * @addtogroup wrapped.cmpxchg
+ *
+ * @brief
+ *   Defines a function which implements an atomic cmpxchg operation which
+ *   cannot spuriously fail using cmpxchg_weak as the underlying atomic
+ *   operation.
+ *
+ * @details
+ *   The defined function's signature will match either patomic_opsig_cmpxchg_t
+ *   or patomic_opsig_explicit_cmpxchg_t (depending on the value of 'vis_p').
+ *
+ * @param atomic_type
+ *   The type of the object on which the atomic operation is to be performed.
+ *   Must not be a VLA or an array of unknown size.
+ *
+ * @param type
+ *   The non-atomic counterpart of 'atomic_type'. This must have the same size
+ *   as 'atomic_type' and must not have a stricter alignment.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param vis_p
+ *   Either the macro 'SHOW_P' if the function should be defined as taking a
+ *   memory order parameter (a.k.a. explicit), or the macro 'HIDE_P' if it
+ *   should not (a.k.a. implicit).
+ *
+ * @param inv
+ *   Either the macro 'HIDE' if 'vis_p' is 'SHOW_P', or the macro 'SHOW' if
+ *   'vis_p' is 'HIDE_P'.
+ *
+ * @param order
+ *   The literal token 'order' if 'vis_p' is 'SHOW_P', otherwise the desired
+ *   memory order to be used implicitly by the atomic operation.
+ *
+ * @param do_atomic_cmpxchg_weak_explicit
+ *   A macro, M, callable as 'M(type, obj, exp, des, succ, fail, ok);' in block
+ *   scope.
+ *
+ *   The arguments provided are:
+ *   - 'type' : forwarded directly from the 'type' argument in this macro
+ *   - 'obj'  : an expression of type 'volatile atomic_type *'
+ *   - 'exp'  : the name of an identifier designating an object of type 'type'
+ *   - 'des'  : the name of an identifier designating an object of type 'type'
+ *   - 'succ' : an expression of type 'int' whose value is a valid memory order
+ *   - 'fail' : an expression of type 'int' whose value is a valid load memory
+ *              order not stronger than 'succ'
+ *   - 'ok'   : the name of an identifier designating an object of type 'int'
+ *
+ *   The expected behaviour of calling the macro M as above is:
+ *   - the values of 'exp' and 'des' are read
+ *   - in a single atomic operation, the value of the object pointed to by
+ *     'obj' is read and, if it compares equal to the value of 'exp', the
+ *     value of 'des' is written to the object pointed to by 'obj'
+ *   - the value read from 'obj' is stored in 'exp'
+ *   - 'ok' is set to non-zero if the value of 'des' was written to the object
+ *     pointed to by 'obj' (success), otherwise it is set to zero (failure)
+ *   - the atomic operation uses a memory ordering at least as strong as 'succ'
+ *     for a successful exchange, and a load memory ordering at least as strong
+ *     as 'fail' for a failed exchange
+ *
+ * @param do_cmp_eq
+ *   A macro, C, callable as 'C(type, a, b, cmp);' in block scope.
+ *
+ *   The arguments provided are:
+ *   - 'type' : forwarded directly from the 'type' argument in this macro
+ *   - 'a'    : the name of an identifier designating an object of type 'type'
+ *   - 'b'    : the name of an identifier designating an object of type 'type'
+ *   - 'cmp'  : the name of an identifier designating an object of type 'int'
+ *
+ *   The expected behaviour of calling the macro C as above is:
+ *   - 'cmp' is set to a non-zero value if 'a' and 'b' compare equal, otherwise
+ *     it is set to zero
+ */
+#define PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_CMPXCHG_STRONG(                   \
+    atomic_type, type, fn_name, vis_p, inv, order,                          \
+    do_atomic_cmpxchg_weak_explicit, do_cmp_eq                              \
+)                                                                           \
+    static int                                                              \
+    fn_name(                                                                \
+        volatile void *const obj                                            \
+        ,void *const expected                                               \
+        ,const void *const desired                                          \
+ vis_p(_,const int succ)                                                    \
+ vis_p(_,const int fail)                                                    \
+    )                                                                       \
+    {                                                                       \
+        /* static assertions */                                             \
+        PATOMIC_STATIC_ASSERT(                                              \
+            sizeof_type_eq_atype, sizeof(type) == sizeof(atomic_type));     \
+                                                                            \
+        /* declarations */                                                  \
+        type exp;                                                           \
+        type des;                                                           \
+        type old;                                                           \
+        int cmp = 0;                                                        \
+        int ok = 0;                                                         \
+    inv(int succ = (int) order;)                                            \
+    inv(int fail = PATOMIC_CMPXCHG_FAIL_ORDER((int) order);)                \
+                                                                            \
+        /* assertions */                                                    \
+        PATOMIC_WRAPPED_DO_ASSERT(obj != NULL);                             \
+        PATOMIC_WRAPPED_DO_ASSERT(expected != NULL);                        \
+        PATOMIC_WRAPPED_DO_ASSERT(desired != NULL);                         \
+        PATOMIC_WRAPPED_DO_ASSERT_ALIGNED(obj, atomic_type);                \
+        PATOMIC_WRAPPED_DO_ASSERT(PATOMIC_IS_VALID_ORDER(succ));            \
+        PATOMIC_WRAPPED_DO_ASSERT(PATOMIC_IS_VALID_FAIL_ORDER(succ, fail)); \
+                                                                            \
+        /* inputs */                                                        \
+        PATOMIC_WRAPPED_DO_MEMCPY(&exp, expected, sizeof(type));            \
+        PATOMIC_WRAPPED_DO_MEMCPY(&des, desired, sizeof(type));             \
+        PATOMIC_WRAPPED_DO_MEMCPY(&old, &exp, sizeof(type));                \
+                                                                            \
+        /* operation */                                                     \
+        do {                                                                \
+            /* cmpxchg_weak */                                              \
+            do_atomic_cmpxchg_weak_explicit(                                \
+                type                                                        \
+                (volatile atomic_type *) obj,                               \
+                exp, des,                                                   \
+                succ, fail,                                                 \
+                ok                                                          \
+            );                                                              \
+                                                                            \
+            /* check for modification */                                    \
+            do_cmp_eq(                                                      \
+                type,                                                       \
+                exp, old,                                                   \
+                cmp                                                         \
+            );                                                              \
+                                                                            \
+            /* was modified */                                              \
+            if (!cmp)                                                       \
+            {                                                               \
+                break;                                                      \
+            }                                                               \
+                                                                            \
+            /* spurious failure */                                          \
+            /* save loaded value for future comparison */                   \
+            else                                                            \
+            {                                                               \
+                PATOMIC_WRAPPED_DO_MEMCPY(&old, &exp, sizeof(type));        \
+            }                                                               \
+        }                                                                   \
+        while (!ok);                                                        \
+                                                                            \
+        /* outputs */                                                       \
+        PATOMIC_WRAPPED_DO_MEMCPY(expected, &exp, sizeof(type));            \
+        return ok != 0;                                                     \
+    }
+
+
 #endif  /* PATOMIC_WRAPPED_CMPXCHG_H */
