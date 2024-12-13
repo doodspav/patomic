f4a95bb7643057d75428da3be264a355cf11310c
doodspav <doodspav@gmail.com>
2024-12-13 12:11:03 +0000

    GHI #37 Improve bt logic inc

--- test/kind/bt/logic/arithmetic/inc.cpp
+++ test/kind/bt/logic/arithmetic/inc.cpp
@@ -1,4 +1,5 @@
 #include <test/common/generic_int.hpp>
+#include <test/common/skip.hpp>
 
 #include <test/suite/bt_logic.hpp>
 
@@ -65,3 +66,123 @@ test_inc(
 }
 
 }  // namespace
+
+
+#define SKIP_NULL_OP_FP_INC(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_inc, "inc")
+
+#define SKIP_NULL_OP_FP_FETCH_INC(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_fetch_inc, "fetch_inc")
+
+
+/// @brief Check that the non-atomic logic of implicit inc works correctly.
+TEST_P(BtLogicImplicit, fp_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_INC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_inc = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_inc(object);
+    };
+
+    // test
+    test_inc(p.width, m_align.recommended, fp_inc);
+}
+
+/// @brief Check that the non-atomic logic of implicit fetch_inc works correctly.
+TEST_P(BtLogicImplicit, fp_fetch_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_INC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_inc = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_inc(object, ret);
+    };
+
+    // test
+    test_fetch_inc(p.width, m_align.recommended, fp_fetch_inc);
+}
+
+
+/// @brief Check that the non-atomic logic of explicit inc works correctly.
+TEST_P(BtLogicExplicit, fp_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_INC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_inc = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_inc(object, p.order);
+    };
+
+    // test
+    test_inc(p.width, m_align.recommended, fp_inc);
+}
+
+/// @brief Check that the non-atomic logic of explicit fetch_inc works correctly.
+TEST_P(BtLogicExplicit, fp_fetch_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_INC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_inc = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_inc(object, p.order, ret);
+    };
+
+    // test
+    test_fetch_inc(p.width, m_align.recommended, fp_fetch_inc);
+}
+
+
+/// @brief Check that the non-atomic logic of transaction inc works correctly.
+TEST_P(BtLogicTransaction, fp_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_INC(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_inc = [&](void *object) -> void {
+            return m_ops.arithmetic_ops.fp_inc(object, m_config, nullptr);
+        };
+
+        // test
+        test_inc(width, 1u, fp_inc);
+    }
+}
+
+/// @brief Check that the non-atomic logic of transaction fetch_inc works correctly.
+TEST_P(BtLogicTransaction, fp_fetch_inc)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_INC(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_fetch_inc = [&](void *object, void *ret) -> void {
+            return m_ops.arithmetic_ops.fp_fetch_inc(object, ret, m_config, nullptr);
+        };
+
+        // test
+        test_fetch_inc(width, 1u, fp_fetch_inc);
+    }
+}
