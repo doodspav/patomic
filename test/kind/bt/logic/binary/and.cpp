aac3919ab44dc61180c33fc49243e686d621d211
doodspav <doodspav@gmail.com>
2024-12-13 06:57:50 +0000

    GHI #37 Implement BtLogicBinaryAnd

--- test/kind/bt/logic/binary/and.cpp
+++ test/kind/bt/logic/binary/and.cpp
@@ -17,7 +17,77 @@ test_bin_fetch_and(
     )>& fp_fetch_and
 )
 {
-    // todo
+    // start with all bits unset
+    test::generic_integer object { width, align, false };
+    test::generic_integer argument { width, align, false };
+    test::generic_integer ret { width, align, false };
+    test::generic_integer object_old { width, align, false };
+    test::generic_integer object_exp { width, align, false };
+    test::generic_integer argument_old { width, align, false };
+
+    // go through all bit offsets
+    for (std::size_t i = 0; i < object.bit_width(); ++i)
+    {
+        for (std::size_t j = 0; j < object.bit_width(); ++j)
+        {
+            // set up params
+            object.inv_at(i);
+            object_old = object;
+            argument.inv_at(j);
+            argument_old = argument;
+
+            // set up expected
+            object_exp.store_zero();
+            if (i == j)
+            {
+                object_exp.inv_at(i);
+            }
+
+            // test
+            fp_fetch_and(object, argument, ret);
+            ASSERT_EQ(object, object_exp);
+            ASSERT_EQ(argument, argument_old);
+            ASSERT_EQ(ret, object_old);
+
+            // reset
+            argument.inv_at(j);
+            object = argument;
+        }
+    }
+
+    // start with all bits set
+    object.inv();
+    argument.inv();
+
+    // go through all bit offsets
+    for (std::size_t i = 0; i < object.bit_width(); ++i)
+    {
+        for (std::size_t j = 0; j < object.bit_width(); ++j)
+        {
+            // set up params
+            object.inv_at(i);
+            object_old = object;
+            argument.inv_at(j);
+            argument_old = argument;
+
+            // set up expected
+            object_exp = object;
+            if (i != j)
+            {
+                object_exp.inv_at(j);
+            }
+
+            // test
+            fp_fetch_and(object, argument, ret);
+            ASSERT_EQ(object, object_exp);
+            ASSERT_EQ(argument, argument_old);
+            ASSERT_EQ(ret, object_old);
+
+            // reset
+            argument.inv_at(j);
+            object = argument;
+        }
+    }
 }
 
 void
