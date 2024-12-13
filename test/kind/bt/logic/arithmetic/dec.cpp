e662dc32224adafa64ba68bf531618849fd946ca
doodspav <doodspav@gmail.com>
2024-12-13 12:14:08 +0000

    GHI #37 Improve bt logic inc

--- test/kind/bt/logic/arithmetic/dec.cpp
+++ test/kind/bt/logic/arithmetic/dec.cpp
@@ -1,4 +1,5 @@
 #include <test/common/generic_int.hpp>
+#include <test/common/skip.hpp>
 
 #include <test/suite/bt_logic.hpp>
 
@@ -65,3 +66,123 @@ test_dec(
 }
 
 }  // namespace
+
+
+#define SKIP_NULL_OP_FP_DEC(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_dec, "dec")
+
+#define SKIP_NULL_OP_FP_FETCH_DEC(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_fetch_dec, "fetch_dec")
+
+
+/// @brief Check that the non-atomic logic of implicit dec works correctly.
+TEST_P(BtLogicImplicit, fp_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_DEC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_dec = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_dec(object);
+    };
+
+    // test
+    test_dec(p.width, m_align.recommended, fp_dec);
+}
+
+/// @brief Check that the non-atomic logic of implicit fetch_dec works correctly.
+TEST_P(BtLogicImplicit, fp_fetch_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_DEC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_dec = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_dec(object, ret);
+    };
+
+    // test
+    test_fetch_dec(p.width, m_align.recommended, fp_fetch_dec);
+}
+
+
+/// @brief Check that the non-atomic logic of explicit dec works correctly.
+TEST_P(BtLogicExplicit, fp_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_DEC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_dec = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_dec(object, p.order);
+    };
+
+    // test
+    test_dec(p.width, m_align.recommended, fp_dec);
+}
+
+/// @brief Check that the non-atomic logic of explicit fetch_dec works correctly.
+TEST_P(BtLogicExplicit, fp_fetch_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_DEC(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_dec = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_dec(object, p.order, ret);
+    };
+
+    // test
+    test_fetch_dec(p.width, m_align.recommended, fp_fetch_dec);
+}
+
+
+/// @brief Check that the non-atomic logic of transaction dec works correctly.
+TEST_P(BtLogicTransaction, fp_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_DEC(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_dec = [&](void *object) -> void {
+            return m_ops.arithmetic_ops.fp_dec(object, m_config, nullptr);
+        };
+
+        // test
+        test_dec(width, 1u, fp_dec);
+    }
+}
+
+/// @brief Check that the non-atomic logic of transaction fetch_dec works correctly.
+TEST_P(BtLogicTransaction, fp_fetch_dec)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_DEC(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_fetch_dec = [&](void *object, void *ret) -> void {
+            return m_ops.arithmetic_ops.fp_fetch_dec(object, ret, m_config, nullptr);
+        };
+
+        // test
+        test_fetch_dec(width, 1u, fp_fetch_dec);
+    }
+}
