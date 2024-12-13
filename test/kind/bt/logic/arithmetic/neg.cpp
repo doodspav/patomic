54b20022d4b0711cbb7de9c38d1219304b81a0b0
doodspav <doodspav@gmail.com>
2024-12-13 12:15:08 +0000

    GHI #37 Improve bt logic neg

--- test/kind/bt/logic/arithmetic/neg.cpp
+++ test/kind/bt/logic/arithmetic/neg.cpp
@@ -1,4 +1,5 @@
 #include <test/common/generic_int.hpp>
+#include <test/common/skip.hpp>
 
 #include <test/suite/bt_logic.hpp>
 
@@ -68,3 +69,123 @@ test_neg(
 }
 
 }  // namespace
+
+
+#define SKIP_NULL_OP_FP_NEG(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_neg, "neg")
+
+#define SKIP_NULL_OP_FP_FETCH_NEG(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).arithmetic_ops.fp_fetch_neg, "fetch_neg")
+
+
+/// @brief Check that the non-atomic logic of implicit neg works correctly.
+TEST_P(BtLogicImplicit, fp_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_NEG(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_neg = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_neg(object);
+    };
+
+    // test
+    test_neg(p.width, m_align.recommended, fp_neg);
+}
+
+/// @brief Check that the non-atomic logic of implicit fetch_neg works correctly.
+TEST_P(BtLogicImplicit, fp_fetch_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_NEG(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_neg = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_neg(object, ret);
+    };
+
+    // test
+    test_fetch_neg(p.width, m_align.recommended, fp_fetch_neg);
+}
+
+
+/// @brief Check that the non-atomic logic of explicit neg works correctly.
+TEST_P(BtLogicExplicit, fp_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_NEG(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_neg = [&](void *object) -> void {
+        return m_ops.arithmetic_ops.fp_neg(object, p.order);
+    };
+
+    // test
+    test_neg(p.width, m_align.recommended, fp_neg);
+}
+
+/// @brief Check that the non-atomic logic of explicit fetch_neg works correctly.
+TEST_P(BtLogicExplicit, fp_fetch_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_NEG(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_fetch_neg = [&](void *object, void *ret) -> void {
+        return m_ops.arithmetic_ops.fp_fetch_neg(object, p.order, ret);
+    };
+
+    // test
+    test_fetch_neg(p.width, m_align.recommended, fp_fetch_neg);
+}
+
+
+/// @brief Check that the non-atomic logic of transaction neg works correctly.
+TEST_P(BtLogicTransaction, fp_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_NEG(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_neg = [&](void *object) -> void {
+            return m_ops.arithmetic_ops.fp_neg(object, m_config, nullptr);
+        };
+
+        // test
+        test_neg(width, 1u, fp_neg);
+    }
+}
+
+/// @brief Check that the non-atomic logic of transaction fetch_neg works correctly.
+TEST_P(BtLogicTransaction, fp_fetch_neg)
+{
+    // check pre-conditions
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_FETCH_NEG(p.id, m_ops);
+
+    // go through all widths
+    for (std::size_t width : p.widths)
+    {
+        // setup
+        m_config.width = width;
+
+        // wrap operation
+        const auto fp_fetch_neg = [&](void *object, void *ret) -> void {
+            return m_ops.arithmetic_ops.fp_fetch_neg(object, ret, m_config, nullptr);
+        };
+
+        // test
+        test_fetch_neg(width, 1u, fp_fetch_neg);
+    }
+}
