26a1bffd0c90f01d5fb482fea75a47cb2c84c866
doodspav <doodspav@gmail.com>
2024-12-14 17:16:24 +0000

    GHI #37 Split up flag tests

---
+++ test/kind/bt/logic/flag/clear.cpp
@@ -0,0 +1,32 @@
+#include <test/common/skip.hpp>
+
+#include <test/suite/bt_logic.hpp>
+
+
+#define SKIP_NULL_OP_FP_CLEAR(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).flag_ops.fp_clear, "clear")
+
+
+TEST_P(BtLogicTransaction, fp_clear)
+{
+    // check pre-condition
+    const auto& p = GetParam();
+    SKIP_NULL_OP_FP_CLEAR(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_clear = [=](patomic_transaction_flag_t& flag) -> void {
+        m_ops.flag_ops.fp_clear(&flag);
+    };
+
+    // setup
+    patomic_transaction_flag_t flag {};
+
+    // clear: unset -> unset
+    fp_clear(flag);
+    ASSERT_EQ(0, flag);
+
+    // clear: set -> unset
+    flag = 1;
+    fp_clear(flag);
+    ASSERT_EQ(0, flag);
+}
---
+++ test/kind/bt/logic/flag/test_set.cpp
@@ -0,0 +1,31 @@
+#include <test/common/skip.hpp>
+
+#include <test/suite/bt_logic.hpp>
+
+
+#define SKIP_NULL_OP_FP_TEST_SET(id, ops) \
+    SKIP_NULL_OP_FP(id, (ops).flag_ops.fp_test_set, "test_set")
+
+
+TEST_P(BtLogicTransaction, fp_test_set)
+{
+    // check pre-condition
+    const auto &p = GetParam();
+    SKIP_NULL_OP_FP_TEST_SET(p.id, m_ops);
+
+    // wrap operation
+    const auto fp_test_set = [=](patomic_transaction_flag_t &flag) -> int {
+        return m_ops.flag_ops.fp_test_set(&flag);
+    };
+
+    // setup
+    patomic_transaction_flag_t flag{};
+
+    // test_set: unset -> set
+    ASSERT_EQ(0, fp_test_set(flag));
+    ASSERT_EQ(1, flag);
+
+    // test_set: set -> set
+    ASSERT_EQ(1, fp_test_set(flag));
+    ASSERT_EQ(1, flag);
+}
