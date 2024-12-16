c739e18228589bf2e90bd32b1d9b067cd8496461
doodspav <doodspav@gmail.com>
2024-12-16 14:20:18 +0000

    GHI #40 Wrap tsx fp_exchange

--- src/include/patomic/wrapped/tsx.h
+++ src/include/patomic/wrapped/tsx.h
@@ -155,4 +155,82 @@
     }
 
 
+/**
+ * @addtogroup wrapped.tsx
+ *
+ * @brief
+ *   Defines a function which implements an atomic exchange operation using
+ *   tbegin and tcommit as the underlying atomic transaction primitives.
+ *
+ * @details
+ *   The defined function's signature will match
+ *   patomic_opsig_transaction_exchange_t.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param tbegin
+ *   A callable with the signature and semantics of
+ *   patomic_opsig_transaction_tbegin_t.
+ *
+ * @param tcommit
+ *   A callable with the signature and semantics of
+ *   patomic_opsig_transaction_tcommit_t.
+ */
+#define PATOMIC_WRAPPED_TSX_DEFINE_OP_EXCHANGE(                             \
+    fn_name, tbegin, tcommit                                                \
+)                                                                           \
+    static void                                                             \
+    fn_name(                                                                \
+        volatile void *const obj,                                           \
+        const void *const desired,                                          \
+        void *const ret,                                                    \
+        patomic_transaction_config_t config,                                \
+        patomic_transaction_result_t *const result                          \
+    )                                                                       \
+    {                                                                       \
+        /* declarations */                                                  \
+        patomic_transaction_result_t res = {0};                             \
+        patomic_transaction_padded_flag_holder_abi_unstable_t holder = {0}; \
+        if (config.flag_nullable == NULL)                                   \
+        {                                                                   \
+            config.flag_nullable = &holder.flag;                            \
+        }                                                                   \
+                                                                            \
+        /* assert result early */                                           \
+        PATOMIC_WRAPPED_DO_ASSERT(result != NULL);                          \
+                                                                            \
+        /* check zero */                                                    \
+        PATOMIC_WRAPPED_TSX_CHECK_CONFIG_ZERO(config, res, cleanup);        \
+                                                                            \
+        /* assertions */                                                    \
+        PATOMIC_WRAPPED_DO_ASSERT(obj != NULL);                             \
+        PATOMIC_WRAPPED_DO_ASSERT(desired != NULL);                         \
+        PATOMIC_WRAPPED_DO_ASSERT(ret != NULL);                             \
+                                                                            \
+        /* operation */                                                     \
+        while (config.attempts-- > 0ul)                                     \
+        {                                                                   \
+            ++res.attempts_made;                                            \
+            res.status = tbegin();                                          \
+            if (res.status == 0ul)                                          \
+            {                                                               \
+                PATOMIC_IGNORE_UNUSED(*config.flag_nullable);               \
+                PATOMIC_WRAPPED_DO_MEMCPY(                                  \
+                    ret, (const void *) obj, config.width                   \
+                );                                                          \
+                PATOMIC_WRAPPED_DO_MEMCPY(                                  \
+                    (void *) obj, desired, config.width                     \
+                );                                                          \
+                tcommit();                                                  \
+                break;                                                      \
+            }                                                               \
+        }                                                                   \
+                                                                            \
+        /* cleanup */                                                       \
+    cleanup:                                                                \
+        *result = res;                                                      \
+    }
+
+
 #endif  /* PATOMIC_WRAPPED_TSX_H */
