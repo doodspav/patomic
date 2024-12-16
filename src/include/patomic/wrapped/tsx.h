b81a88604e3902c4701807fa1785122060c886f2
doodspav <doodspav@gmail.com>
2024-12-16 21:53:24 +0000

    GHI #40 Add tsx wrap for patomic_opsig_transaction_generic_wfb_t

--- src/include/patomic/wrapped/tsx.h
+++ src/include/patomic/wrapped/tsx.h
@@ -1934,51 +1934,155 @@
  *   A callable with the signature and semantics of
  *   patomic_opsig_transaction_tcommit_t.
  */
-#define PATOMIC_WRAPPED_TSX_DEFINE_OP_GENERIC(                            \
-    fn_name, tbegin, tcommit                                              \
-)                                                                         \
-    static void                                                           \
-    fn_name(                                                              \
-        void (* const fn) (void *),                                       \
-        void *const ctx,                                                  \
-        patomic_transaction_config_t config,                              \
-        patomic_transaction_result_t *const result                        \
-    )                                                                     \
-    {                                                                     \
-        /* declarations */                                                \
-        patomic_transaction_result_t res = {0};                           \
-        const patomic_transaction_flag_t flag = 0;                        \
-        if (config.flag_nullable == NULL)                                 \
-        {                                                                 \
-            config.flag_nullable = &flag;                                 \
-        }                                                                 \
-                                                                          \
-        /* assert early */                                                \
-        PATOMIC_WRAPPED_DO_ASSERT(result != NULL);                        \
-                                                                          \
-        /* check zero */                                                  \
-        PATOMIC_WRAPPED_TSX_CHECK_CONFIG_ZERO(config, res, cleanup);      \
-                                                                          \
-        /* assertions */                                                  \
-        PATOMIC_WRAPPED_DO_ASSERT(fn != NULL);                            \
-                                                                          \
-        /* operation */                                                   \
-        while (config.attempts-- > 0ul)                                   \
-        {                                                                 \
-            ++res.attempts_made;                                          \
-            res.status = tbegin();                                        \
-            if (res.status == 0ul)                                        \
-            {                                                             \
-                PATOMIC_IGNORE_UNUSED(*config.flag_nullable);             \
-                fn(ctx);                                                  \
-                tcommit();                                                \
-                goto cleanup;                                             \
-            }                                                             \
-        }                                                                 \
-                                                                          \
-        /* cleanup */                                                     \
-    cleanup:                                                              \
-        *result = res;                                                    \
+#define PATOMIC_WRAPPED_TSX_DEFINE_OP_GENERIC(                       \
+    fn_name, tbegin, tcommit                                         \
+)                                                                    \
+    static void                                                      \
+    fn_name(                                                         \
+        void (*const fn) (void *),                                   \
+        void *const ctx,                                             \
+        patomic_transaction_config_t config,                         \
+        patomic_transaction_result_t *const result                   \
+    )                                                                \
+    {                                                                \
+        /* declarations */                                           \
+        patomic_transaction_result_t res = {0};                      \
+        const patomic_transaction_flag_t flag = 0;                   \
+        if (config.flag_nullable == NULL)                            \
+        {                                                            \
+            config.flag_nullable = &flag;                            \
+        }                                                            \
+                                                                     \
+        /* assert early */                                           \
+        PATOMIC_WRAPPED_DO_ASSERT(result != NULL);                   \
+                                                                     \
+        /* check zero */                                             \
+        PATOMIC_WRAPPED_TSX_CHECK_CONFIG_ZERO(config, res, cleanup); \
+                                                                     \
+        /* assertions */                                             \
+        PATOMIC_WRAPPED_DO_ASSERT(fn != NULL);                       \
+                                                                     \
+        /* operation */                                              \
+        while (config.attempts-- > 0ul)                              \
+        {                                                            \
+            ++res.attempts_made;                                     \
+            res.status = tbegin();                                   \
+            if (res.status == 0ul)                                   \
+            {                                                        \
+                PATOMIC_IGNORE_UNUSED(*config.flag_nullable);        \
+                fn(ctx);                                             \
+                tcommit();                                           \
+                goto cleanup;                                        \
+            }                                                        \
+        }                                                            \
+                                                                     \
+        /* cleanup */                                                \
+    cleanup:                                                         \
+        *result = res;                                               \
+    }
+
+
+/**
+ * @addtogroup wrapped.tsx
+ *
+ * @brief
+ *   Defines a function which implements an atomic generic_wfb operation using
+ *   tbegin and tcommit as the underlying atomic transaction primitives.
+ *
+ * @details
+ *   The defined function's signature will match
+ *   patomic_opsig_transaction_generic_wfb_t.
+ *
+ * @param fn_name
+ *   The name of the function to be defined.
+ *
+ * @param tbegin
+ *   A callable with the signature and semantics of
+ *   patomic_opsig_transaction_tbegin_t.
+ *
+ * @param tcommit
+ *   A callable with the signature and semantics of
+ *   patomic_opsig_transaction_tcommit_t.
+ */
+#define PATOMIC_WRAPPED_TSX_DEFINE_OP_GENERIC_WFB(                           \
+    fn_name, tbegin, tcommit                                                 \
+)                                                                            \
+    static int                                                               \
+    fn_name(                                                                 \
+        void (*const fn) (void *),                                           \
+        void *const ctx,                                                     \
+        void (*const fallback_fn) (void *),                                  \
+        void *const fallback_ctx,                                            \
+        patomic_transaction_config_wfb_t config,                             \
+        patomic_transaction_result_wfb_t *const result                       \
+    )                                                                        \
+    {                                                                        \
+        /* declarations */                                                   \
+        patomic_transaction_result_wfb_t res = {0};                          \
+        const patomic_transaction_flag_t flag = 0;                           \
+        if (config.flag_nullable == NULL)                                    \
+        {                                                                    \
+            config.flag_nullable = &flag;                                    \
+        }                                                                    \
+        if (config.fallback_flag_nullable == NULL)                           \
+        {                                                                    \
+            config.fallback_flag_nullable = &flag;                           \
+        }                                                                    \
+                                                                             \
+        /* assert early */                                                   \
+        PATOMIC_WRAPPED_DO_ASSERT(result != NULL);                           \
+                                                                             \
+        /* check zero */                                                     \
+        PATOMIC_WRAPPED_TSX_CHECK_CONFIG_ZERO_WFB(                           \
+            config, res, fallback, cleanup                                   \
+        );                                                                   \
+                                                                             \
+        /* assertions */                                                     \
+        PATOMIC_WRAPPED_DO_ASSERT(fn != NULL);                               \
+                                                                             \
+        /* operation */                                                      \
+        while (config.attempts-- > 0ul)                                      \
+        {                                                                    \
+            ++res.attempts_made;                                             \
+            res.status = tbegin();                                           \
+            if (res.status == 0ul)                                           \
+            {                                                                \
+                PATOMIC_IGNORE_UNUSED(*config.flag_nullable);                \
+                fn(ctx);                                                     \
+                tcommit();                                                   \
+                goto cleanup;                                                \
+            }                                                                \
+        }                                                                    \
+                                                                             \
+        /* fallback */                                                       \
+    fallback:                                                                \
+                                                                             \
+        /* check zero */                                                     \
+        PATOMIC_WRAPPED_TSX_CHECK_CONFIG_ZERO_FALLBACK(                      \
+            config, res, cleanup                                             \
+        );                                                                   \
+                                                                             \
+        /* assertions */                                                     \
+        PATOMIC_WRAPPED_DO_ASSERT(fallback_fn != NULL);                      \
+                                                                             \
+        /* operation */                                                      \
+        while (config.fallback_attempts-- > 0ul)                             \
+        {                                                                    \
+            ++res.fallback_attempts_made;                                    \
+            res.fallback_status = tbegin();                                  \
+            if (res.fallback_status == 0ul)                                  \
+            {                                                                \
+                PATOMIC_IGNORE_UNUSED(*config.fallback_flag_nullable);       \
+                fallback_fn(fallback_ctx);                                   \
+                tcommit();                                                   \
+                goto cleanup;                                                \
+            }                                                                \
+        }                                                                    \
+                                                                             \
+        /* cleanup */                                                        \
+    cleanup:                                                                 \
+        *result = res;                                                       \
+        return res.status == 0ul;                                            \
     }
 
 
