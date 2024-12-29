caccb0c7ba91a7d35a3aeb95703437e4e5a51335
doodspav <doodspav@gmail.com>
2024-12-29 23:18:37 +0000

    GHI #47 Add helper macros

---
+++ src/impl/msvc/ops/base/macros.h
@@ -0,0 +1,24 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_BASE_MACROS_H
+#define PATOMIC_IMPL_MSVC_OPS_BASE_MACROS_H
+
+#include "config.h"
+
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    #define VIS_IL_ACQ_REL(expr) expr
+#else
+    #define VIS_IL_ACQ_REL(expr)
+#endif
+
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    #define VIS_IL_NF(expr) expr
+#else
+    #define VIS_IL_NF(expr)
+#endif
+
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BASE_MACROS_H */
