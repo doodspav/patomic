6907b2d8959140c2ce776433a2a223418211a4f1
doodspav <doodspav@gmail.com>
2024-12-30 14:42:27 +0000

    GHI #47 Do xor instead

--- src/impl/msvc/ops/binary/xor.h
+++ src/impl/msvc/ops/binary/xor.h
@@ -1,8 +1,8 @@
 /* Copyright (c) doodspav. */
 /* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
 
-#ifndef PATOMIC_IMPL_MSVC_OPS_BINARY_OR_H
-#define PATOMIC_IMPL_MSVC_OPS_BINARY_OR_H
+#ifndef PATOMIC_IMPL_MSVC_OPS_BINARY_XOR_H
+#define PATOMIC_IMPL_MSVC_OPS_BINARY_XOR_H
 
 #ifdef _MSC_VER
 
@@ -17,4 +17,4 @@
 
 #endif  /* defined(_MSC_VER) */
 
-#endif  /* PATOMIC_IMPL_MSVC_OPS_BINARY_OR_H */
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BINARY_XOR_H */
