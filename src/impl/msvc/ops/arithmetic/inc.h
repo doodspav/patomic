be7ed8c0d1839ea17e1ac32a84825eff150b9ef0
doodspav <doodspav@gmail.com>
2024-12-30 16:49:47 +0000

    GHI #47 Setup inc

---
+++ src/impl/msvc/ops/arithmetic/inc.h
@@ -0,0 +1,24 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_INC_H
+#define PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_INC_H
+
+#ifdef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/macros/ignore_unused.h>
+
+#include <patomic/wrapped/cmpxchg.h>
+#include <patomic/wrapped/direct.h>
+
+#include <patomic/api/ops.h>
+
+
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_INC_H */
