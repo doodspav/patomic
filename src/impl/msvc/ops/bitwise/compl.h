2b59df0f5703e26bb1facc41aff695101834062e
doodspav <doodspav@gmail.com>
2024-12-30 22:07:22 +0000

    GHI #47 Move bti_modify to bitwise

--- src/impl/msvc/ops/bitwise/compl.h
+++ src/impl/msvc/ops/bitwise/compl.h
@@ -1,8 +1,8 @@
 /* Copyright (c) doodspav. */
 /* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
 
-#ifndef PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H
-#define PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H
+#ifndef PATOMIC_IMPL_MSVC_OPS_BITWISE_COMPL_H
+#define PATOMIC_IMPL_MSVC_OPS_BITWISE_COMPL_H
 
 #ifdef _MSC_VER
 
@@ -281,4 +281,4 @@ PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
 
 #endif  /* defined(_MSC_VER) */
 
-#endif  /* PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H */
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BITWISE_COMPL_H */
--- src/impl/msvc/ops/bitwise/set.h
+++ src/impl/msvc/ops/bitwise/set.h
@@ -1,8 +1,8 @@
 /* Copyright (c) doodspav. */
 /* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
 
-#ifndef PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H
-#define PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H
+#ifndef PATOMIC_IMPL_MSVC_OPS_BITWISE_SET_H
+#define PATOMIC_IMPL_MSVC_OPS_BITWISE_SET_H
 
 #ifdef _MSC_VER
 
@@ -282,4 +282,4 @@ PATOMIC_WRAPPED_DIRECT_DEFINE_OP_BIT_TEST_MODIFY(
 
 #endif  /* defined(_MSC_VER) */
 
-#endif  /* PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H */
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BITWISE_SET_H */
