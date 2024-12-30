1ec1c3d5a2b13272bfe2601978ddde8d05ef9cb3
doodspav <doodspav@gmail.com>
2024-12-30 15:17:43 +0000

    GHI #47 Add xor 8

--- src/impl/msvc/ops/binary/xor.h
+++ src/impl/msvc/ops/binary/xor.h
@@ -8,6 +8,8 @@
 
 #include "../base.h"
 
+#include <patomic/macros/ignore_unused.h>
+
 #include <patomic/wrapped/cmpxchg.h>
 #include <patomic/wrapped/direct.h>
 
@@ -40,6 +42,7 @@
     do {                                                        \
         type res;                                               \
         do_fetch_xor_explicit_n(n, type, obj, arg, order, res); \
+        PATOMIC_IGNORE_UNUSED(res);                             \
     }                                                           \
     while (0)
 
@@ -54,6 +57,140 @@
         des = patomic_msvc128_xor(exp, arg);         \
     }                                                \
     while (0)
+
+
+/**
+ * Defines patomic_opimpl_{void, fetch}_xor_8_<order> with order:
+ * - relaxed
+ * - acquire
+ * - release
+ * - seq_cst
+ * - explicit
+ */
+#if PATOMIC_IMPL_MSVC_HAS_IL_XOR_8
+
+char _InterlockedXor8(char volatile *, char);
+#pragma intrinsic(_InterlockedXor8)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    char _InterlockedXor8_nf(char volatile *, char);
+    #pragma intrinsic(_InterlockedXor8_nf)
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    char _InterlockedXor8_acq(char volatile *, char);
+    char _InterlockedXor8_rel(char volatile *, char);
+    #pragma intrinsic(_InterlockedXor8_acq)
+    #pragma intrinsic(_InterlockedXor8_rel)
+#endif
+
+#define do_void_xor_explicit_8(type, obj, arg, order) \
+    do_void_xor_explicit_n(8, type, obj, arg, order)
+
+#define do_fetch_xor_explicit_8(type, obj, arg, order, res) \
+    do_fetch_xor_explicit_n(8, type, obj, arg, order, res)
+
+PATOMIC_WRAPPED_DIRECT_DEFINE_OP_VOID(
+    char, char, patomic_opimpl_void_xor_8_explicit,
+    SHOW_P, order, do_void_xor_explicit_8
+)
+PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(
+    char, char, patomic_opimpl_fetch_xor_8_explicit,
+    SHOW_P, order, do_fetch_xor_explicit_8
+)
+
+PATOMIC_WRAPPED_DIRECT_DEFINE_OP_VOID(
+    char, char, patomic_opimpl_void_xor_8_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_void_xor_explicit_8
+)
+PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(
+    char, char, patomic_opimpl_fetch_xor_8_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_fetch_xor_explicit_8
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_acquire,
+        HIDE_P, patomic_ACQUIRE, do_void_xor_explicit_8
+    )
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_acquire,
+        HIDE_P, patomic_ACQUIRE, do_fetch_xor_explicit_8
+    )
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_release,
+        HIDE_P, patomic_RELEASE, do_void_xor_explicit_8
+    )
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_release,
+        HIDE_P, patomic_RELEASE, do_fetch_xor_explicit_8
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_relaxed,
+        HIDE_P, patomic_RELAXED, do_void_xor_explicit_8
+    )
+    PATOMIC_WRAPPED_DIRECT_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_relaxed,
+        HIDE_P, patomic_RELAXED, do_fetch_xor_explicit_8
+    )
+#endif
+
+#elif PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_8
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_VOID(
+    char, char, patomic_opimpl_void_xor_8_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_8, do_make_desired_xor
+)
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_FETCH(
+    char, char, patomic_opimpl_fetch_xor_8_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_8, do_make_desired_xor
+)
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_VOID(
+    char, char, patomic_opimpl_void_xor_8_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_8, do_make_desired_xor
+)
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_FETCH(
+    char, char, patomic_opimpl_fetch_xor_8_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_8, do_make_desired_xor
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_VOID(
+        char, char, patomic_opimpl_void_xor_8_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_FETCH(
+        char, char, patomic_opimpl_fetch_xor_8_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_8, do_make_desired_xor
+    )
+#endif
+
+#endif  /* PATOMIC_IMPL_MSVC_HAS_IL_XOR_8 */
+
+
 
 
 #endif  /* defined(_MSC_VER) */
