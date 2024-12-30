6f7c96341af1f636a75e954f5d68f545fee3c22d
doodspav <doodspav@gmail.com>
2024-12-30 01:17:19 +0000

    GHI #47 Fix m128 ops to work as signed

--- src/impl/msvc/ops/base/m128.h
+++ src/impl/msvc/ops/base/m128.h
@@ -4,6 +4,8 @@
 #ifndef PATOMIC_IMPL_MSVC_OPS_BASE_M128_H
 #define PATOMIC_IMPL_MSVC_OPS_BASE_M128_H
 
+#define _MSC_VER
+#define __int64 long long
 #ifdef _MSC_VER
 
 #include <patomic/macros/static_assert.h>
@@ -20,11 +22,11 @@
  *   check for endianness.
  */
 typedef struct {
-    unsigned __int64 low;
-    unsigned __int64 high;
-} patomic_mscv128_t;
+    __int64 low;
+    __int64 high;
+} patomic_msvc128_t;
 
-PATOMIC_STATIC_ASSERT(mscv128_size, (sizeof(patomic_mscv128_t) == 16u));
+PATOMIC_STATIC_ASSERT(msvc128_size, (sizeof(patomic_msvc128_t) == 16u));
 
 
 /**
@@ -36,23 +38,25 @@ PATOMIC_STATIC_ASSERT(mscv128_size, (sizeof(patomic_mscv128_t) == 16u));
  * @pre
  *   offset >= 0 && offset < 128
  */
-static patomic_mscv128_t
+static patomic_msvc128_t
 patomic_msvc128_shift_left(
-    const patomic_mscv128_t obj,
+    const patomic_msvc128_t obj,
     int offset
 )
 {
-    patomic_mscv128_t res = {0};
+    const unsigned __int64 obj_low = (unsigned __int64) obj.low;
+    const unsigned __int64 obj_high = (unsigned __int64) obj.high;
+    patomic_msvc128_t res = {0};
 
     if (offset < 64)
     {
-        res.low = obj.low << offset;
-        res.high = (obj.high << offset) | (obj.low >> (64 - offset));
+        res.low = (__int64) (obj_low << offset);
+        res.high = (__int64) ((obj_high << offset) | (obj_low >> (64 - offset)));
     }
     else
     {
         res.low = 0;
-        res.high = obj.low << (offset - 64);
+        res.high = (__int64) (obj_low << (offset - 64));
     }
 
     return res;
@@ -65,17 +69,21 @@ patomic_msvc128_shift_left(
  * @brief
  *   Implement addition operator for patomic_msvc128_t.
  */
-static patomic_mscv128_t
+static patomic_msvc128_t
 patomic_msvc128_add(
-    const patomic_mscv128_t lhs,
-    const patomic_mscv128_t rhs
+    const patomic_msvc128_t lhs,
+    const patomic_msvc128_t rhs
 )
 {
-    patomic_mscv128_t res = {0};
+    const unsigned __int64 lhs_low = (unsigned __int64) lhs.low;
+    const unsigned __int64 lhs_high = (unsigned __int64) lhs.high;
+    const unsigned __int64 rhs_low = (unsigned __int64) rhs.low;
+    const unsigned __int64 rhs_high = (unsigned __int64) rhs.high;
+    patomic_msvc128_t res = {0};
 
-    res.low = lhs.low + rhs.low;
-    unsigned __int64 carry = (res.low < lhs.low) ? 1 : 0;
-    res.high = lhs.high + rhs.high + carry;
+    res.low = (__int64) (lhs_low + rhs_low);
+    unsigned __int64 carry = (((unsigned __int64) res.low) < lhs_low) ? 1 : 0;
+    res.high = (__int64) (lhs_high + rhs_high + carry);
 
     return res;
 }
