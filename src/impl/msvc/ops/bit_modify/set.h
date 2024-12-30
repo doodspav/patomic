ba1bb8665bec2a8556cab3fa28083bb842d3ad44
doodspav <doodspav@gmail.com>
2024-12-30 14:21:24 +0000

    GHI #47 Setup set

---
+++ src/impl/msvc/ops/bit_modify/set.h
@@ -0,0 +1,25 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H
+#define PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H
+
+#ifdef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/stdlib/stdint.h>
+
+#include <patomic/wrapped/direct.h>
+
+#include <patomic/api/ops.h>
+
+#include <limits.h>
+
+
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_SET_H */
