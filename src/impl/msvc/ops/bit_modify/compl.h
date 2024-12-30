6ec43fa66b82e98504debbc2b78e5dc7b3dc8a7d
doodspav <doodspav@gmail.com>
2024-12-30 11:47:39 +0000

    GHI #47 Minimal compl setup

---
+++ src/impl/msvc/ops/bit_modify/compl.h
@@ -0,0 +1,21 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H
+#define PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H
+
+#ifndef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/wrapped/cmpxchg.h>
+
+#include <patomic/api/ops.h>
+
+
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H */
