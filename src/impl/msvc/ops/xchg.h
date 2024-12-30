3ba6d11efaccf922fa29ee4496a53378b0e29aed
doodspav <doodspav@gmail.com>
2024-12-30 01:25:45 +0000

    GHI #47 Tidy up file

--- src/impl/msvc/ops/xchg.h
+++ src/impl/msvc/ops/xchg.h
@@ -1,15 +1,14 @@
 /* Copyright (c) doodspav. */
 /* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
 
-#ifndef PATOMIC_IMPL_MSVC_OPS_XCHG_H
-#define PATOMIC_IMPL_MSVC_OPS_XCHG_H
+#ifndef PATOMIC_IMPL_MSVC_OPS_CMPXCHG_H
+#define PATOMIC_IMPL_MSVC_OPS_CMPXCHG_H
 
 #ifdef _MSC_VER
 
 #include "base.h"
 
 #include <patomic/wrapped/direct.h>
-#include <patomic/wrapped/cmpxchg.h>
 
 #include <patomic/api/ops.h>
 
@@ -446,4 +445,4 @@ PATOMIC_WRAPPED_DIRECT_DEFINE_OP_CMPXCHG(
 
 #endif  /* ^^ defined(_MSC_VER) */
 
-#endif  /* PATOMIC_IMPL_MSVC_OPS_XCHG_H */
+#endif  /* PATOMIC_IMPL_MSVC_OPS_CMPXCHG_H */
