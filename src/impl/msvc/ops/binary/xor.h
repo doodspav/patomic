8df7b7476cd690654dc8c02442451eb5c49808c2
doodspav <doodspav@gmail.com>
2024-12-30 14:57:43 +0000

    GHI #47 Define do_ macros

--- src/impl/msvc/ops/binary/xor.h
+++ src/impl/msvc/ops/binary/xor.h
@@ -14,6 +14,47 @@
 #include <patomic/api/ops.h>
 
 
+#define do_fetch_xor_explicit_n(n, type, obj, arg, order, res) \
+    do {                                                       \
+        switch (order)                                         \
+        {                                                      \
+            case patomic_RELAXED:                              \
+      VIS_IL_NF((res = _InterlockedXor##n##_nf(obj, arg));)    \
+      VIS_IL_NF(break;)                                        \
+            case patomic_CONSUME:                              \
+            case patomic_ACQUIRE:                              \
+     VIS_IL_ACQ((res = _InterlockedXor##n##_acq(obj, arg));)   \
+     VIS_IL_ACQ(break;)                                        \
+            case patomic_RELEASE:                              \
+     VIS_IL_REL((res = _InterlockedXor##n##_rel(obj, arg));)   \
+     VIS_IL_REL(break;)                                        \
+            case patomic_ACQ_REL:                              \
+            case patomic_SEQ_CST:                              \
+            default:                                           \
+                res = _InterlockedXor##n(obj, arg);            \
+        }                                                      \
+    }                                                          \
+    while (0)
+
+#define do_void_xor_explicit_n(n, type, obj, arg, order)        \
+    do {                                                        \
+        type res;                                               \
+        do_fetch_xor_explicit_n(n, type, obj, arg, order, res); \
+    }                                                           \
+    while (0)
+
+#define do_make_desired_xor(type, exp, arg, des)                      \
+    do {                                                              \
+        des = (type) (((unsigned type) exp) | ((unsigned type) arg)); \
+    }                                                                 \
+    while (0)
+
+#define do_make_desired_xor_128(type, exp, arg, des) \
+    do {                                             \
+        des = patomic_msvc128_xor(exp, arg);         \
+    }                                                \
+    while (0)
+
 
 #endif  /* defined(_MSC_VER) */
 
