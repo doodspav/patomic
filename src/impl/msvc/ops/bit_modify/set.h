1b418db9307fe1bf830f919d1cf29d5497ad15eb
doodspav <doodspav@gmail.com>
2024-12-30 14:26:00 +0000

    GHI #47 Define do_bit_test_reset_explicit

--- src/impl/msvc/ops/bit_modify/set.h
+++ src/impl/msvc/ops/bit_modify/set.h
@@ -17,7 +17,47 @@
 #include <limits.h>
 
 
-
+/**
+ * @note
+ *   Doing pointer hacks like this is _technically_ undefined behaviour,
+ *   but msvc won't care and I would be surprised if this broke anything.
+ *   It shouldn't be affected by CET or equivalent pointer checks.
+ */
+#define do_bit_test_reset_explicit(type, obj, offset, order, res)            \
+    do {                                                                     \
+        const patomic_intptr_unsigned_t addr =                               \
+            (patomic_intptr_unsigned_t) obj;                                 \
+                                                                             \
+        /* align our address to 4 bytes */                                   \
+        const patomic_intptr_unsigned_t new_addr =                           \
+            (patomic_intptr_unsigned_t) (addr & ~3ul);                       \
+        long * new_obj = (long *) new_addr;                                  \
+        int new_offset = offset;                                             \
+        new_offset += (int) ((addr - new_addr) * CHAR_BIT);                  \
+                                                                             \
+        /* update offset to be less than 32 */                               \
+        new_obj += new_offset / 32;                                          \
+        new_offset %= 32;                                                    \
+                                                                             \
+        switch (order)                                                       \
+        {                                                                    \
+            case patomic_RELAXED:                                            \
+      VIS_IL_NF((res = _interlockedbittestandset_nf(new_obj, new_offset));)  \
+      VIS_IL_NF(break;)                                                      \
+            case patomic_CONSUME:                                            \
+            case patomic_ACQUIRE:                                            \
+     VIS_IL_ACQ((res = _interlockedbittestandset_acq(new_obj, new_offset));) \
+     VIS_IL_ACQ(break;)                                                      \
+            case patomic_RELEASE:                                            \
+     VIS_IL_REL((res = _interlockedbittestandset_rel(new_obj, new_offset));) \
+     VIS_IL_REL(break;)                                                      \
+            case patomic_ACQ_REL:                                            \
+            case patomic_SEQ_CST:                                            \
+            default:                                                         \
+                res = _interlockedbittestandset(new_obj, new_offset);        \
+        }                                                                    \
+    }                                                                        \
+    while (0)
 
 
 #endif  /* defined(_MSC_VER) */
