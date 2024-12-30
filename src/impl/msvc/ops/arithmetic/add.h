5f425844101e56ff8b4efe9ec93efbe2d07888e2
doodspav <doodspav@gmail.com>
2024-12-30 16:16:56 +0000

    GHI #47 Define do_ macros

--- src/impl/msvc/ops/arithmetic/add.h
+++ src/impl/msvc/ops/arithmetic/add.h
@@ -16,6 +16,48 @@
 #include <patomic/api/ops.h>
 
 
+#define do_fetch_add_explicit_n(n, type, obj, arg, order, res)       \
+    do {                                                             \
+        switch (order)                                               \
+        {                                                            \
+            case patomic_RELAXED:                                    \
+      VIS_IL_NF((res = _InterlockedExchangeAdd##n##_nf(obj, arg));)  \
+      VIS_IL_NF(break;)                                              \
+            case patomic_CONSUME:                                    \
+            case patomic_ACQUIRE:                                    \
+     VIS_IL_ACQ((res = _InterlockedExchangeAdd##n##_acq(obj, arg));) \
+     VIS_IL_ACQ(break;)                                              \
+            case patomic_RELEASE:                                    \
+     VIS_IL_REL((res = _InterlockedExchangeAdd##n##_rel(obj, arg));) \
+     VIS_IL_REL(break;)                                              \
+            case patomic_ACQ_REL:                                    \
+            case patomic_SEQ_CST:                                    \
+            default:                                                 \
+                res = _InterlockedExchangeAdd##n(obj, arg);          \
+        }                                                            \
+    }                                                                \
+    while (0)
+
+#define do_void_add_explicit_n(n, type, obj, arg, order)        \
+    do {                                                        \
+        type res;                                               \
+        do_fetch_add_explicit_n(n, type, obj, arg, order, res); \
+        PATOMIC_IGNORE_UNUSED(res);                             \
+    }                                                           \
+    while (0)
+
+#define do_make_desired_add(type, exp, arg, des)                      \
+    do {                                                              \
+        des = (type) (((unsigned type) exp) + ((unsigned type) arg)); \
+    }                                                                 \
+    while (0)
+
+#define do_make_desired_add_128(type, exp, arg, des) \
+    do {                                             \
+        des = patomic_msvc128_add(exp, arg);         \
+    }                                                \
+    while (0)
+
 
 #endif  /* defined(_MSC_VER) */
 
