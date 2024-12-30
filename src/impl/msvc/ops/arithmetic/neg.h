7c544174462c68b2b0fe2221d37bcb39d1c32dc2
doodspav <doodspav@gmail.com>
2024-12-30 21:37:22 +0000

    GHI #47 Setup neg

---
+++ src/impl/msvc/ops/arithmetic/neg.h
@@ -0,0 +1,19 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_NEG_H
+#define PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_NEG_H
+
+#ifdef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/wrapped/cmpxchg.h>
+
+#include <patomic/api/ops.h>
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_ARITHMETIC_NEG_H */
