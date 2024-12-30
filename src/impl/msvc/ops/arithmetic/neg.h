f61aa3349b0ced09a5b4d610827cd045d8ac9b83
doodspav <doodspav@gmail.com>
2024-12-30 21:50:45 +0000

    GHI #47 Add do_ macros

--- src/impl/msvc/ops/arithmetic/neg.h
+++ src/impl/msvc/ops/arithmetic/neg.h
@@ -13,6 +13,22 @@
 #include <patomic/api/ops.h>
 
 
+#define do_make_desired_neg(type, exp, des)                          \
+    do {                                                             \
+        des = (type) (~((unsigned type) exp) + ((unsigned type) 1)); \
+    }                                                                \
+    while (0)
+
+#define do_make_desired_neg_128(type, exp, des)        \
+    do {                                               \
+        patomic_msvc128_t arg = {0};                   \
+        arg.low = 1;                                   \
+        des.low = (type) ~((unsigned type) exp.low);   \
+        des.high = (type) ~((unsigned type) exp.high); \
+        des = patomic_msvc128_add(des, arg);           \
+    }                                                  \
+    while (0)
+
 
 #endif  /* defined(_MSC_VER) */
 
