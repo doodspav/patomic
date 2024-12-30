ab9bfc50062fd74df769d00511eb686816ee6967
doodspav <doodspav@gmail.com>
2024-12-30 12:26:42 +0000

    GHI #47 Add bit test compl 16, 32, 64, 128

--- src/impl/msvc/ops/bit_modify/compl.h
+++ src/impl/msvc/ops/bit_modify/compl.h
@@ -99,6 +99,190 @@ PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
 #endif  /* PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_8 */
 
 
+/**
+ * Defines patomic_opimpl_bit_test_compl_16_<order> with order:
+ * - relaxed
+ * - acquire
+ * - release
+ * - seq_cst
+ * - explicit
+ */
+#if PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_16
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    short, short, patomic_opimpl_bit_test_compl_16_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_16,
+    do_get_bit, do_make_desired_compl
+)
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    short, short, patomic_opimpl_bit_test_compl_16_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_16,
+    do_get_bit, do_make_desired_compl
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        short, short, patomic_opimpl_bit_test_compl_16_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_16,
+        do_get_bit, do_make_desired_compl
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        short, short, patomic_opimpl_bit_test_compl_16_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_16,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        short, short, patomic_opimpl_bit_test_compl_16_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_16,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#endif  /* PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_16 */
+
+
+/**
+ * Defines patomic_opimpl_bit_test_compl_32_<order> with order:
+ * - relaxed
+ * - acquire
+ * - release
+ * - seq_cst
+ * - explicit
+ */
+#if PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_32
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    long, long, patomic_opimpl_bit_test_compl_32_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_32,
+    do_get_bit, do_make_desired_compl
+)
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    long, long, patomic_opimpl_bit_test_compl_32_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_32,
+    do_get_bit, do_make_desired_compl
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        long, long, patomic_opimpl_bit_test_compl_32_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_32,
+        do_get_bit, do_make_desired_compl
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        long, long, patomic_opimpl_bit_test_compl_32_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_32,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        long, long, patomic_opimpl_bit_test_compl_32_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_32,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#endif  /* PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_32 */
+
+
+/**
+ * Defines patomic_opimpl_bit_test_compl_64_<order> with order:
+ * - relaxed
+ * - acquire
+ * - release
+ * - seq_cst
+ * - explicit
+ */
+#if PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_64
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    __int64, __int64, patomic_opimpl_bit_test_compl_64_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_64,
+    do_get_bit, do_make_desired_compl
+)
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    __int64, __int64, patomic_opimpl_bit_test_compl_64_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_64,
+    do_get_bit, do_make_desired_compl
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        __int64, __int64, patomic_opimpl_bit_test_compl_64_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_64,
+        do_get_bit, do_make_desired_compl
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        __int64, __int64, patomic_opimpl_bit_test_compl_64_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_64,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        __int64, __int64, patomic_opimpl_bit_test_compl_64_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_64,
+        do_get_bit, do_make_desired_compl
+    )
+#endif
+
+#endif  /* PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_64 */
+
+
+/**
+ * Defines patomic_opimpl_bit_test_compl_128_<order> with order:
+ * - relaxed
+ * - acquire
+ * - release
+ * - seq_cst
+ * - explicit
+ */
+#if PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_128
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    patomic_msvc128_t, patomic_msvc128_t, patomic_opimpl_bit_test_compl_128_explicit,
+    SHOW_P, order, do_cmpxchg_explicit_128,
+    do_get_bit_128, do_make_desired_compl_128
+)
+
+PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+    patomic_msvc128_t, patomic_msvc128_t, patomic_opimpl_bit_test_compl_128_seq_cst,
+    HIDE_P, patomic_SEQ_CST, do_cmpxchg_explicit_128,
+    do_get_bit_128, do_make_desired_compl_128
+)
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_ACQ_REL
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        patomic_msvc128_t, patomic_msvc128_t, patomic_opimpl_bit_test_compl_128_acquire,
+        HIDE_P, patomic_ACQUIRE, do_cmpxchg_explicit_128,
+        do_get_bit_128, do_make_desired_compl_128
+    )
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        patomic_msvc128_t, patomic_msvc128_t, patomic_opimpl_bit_test_compl_128_release,
+        HIDE_P, patomic_RELEASE, do_cmpxchg_explicit_128,
+        do_get_bit_128, do_make_desired_compl_128
+    )
+#endif
+
+#if PATOMIC_IMPL_MSVC_HAS_IL_NF
+    PATOMIC_WRAPPED_CMPXCHG_DEFINE_OP_BIT_TEST_MODIFY(
+        patomic_msvc128_t, patomic_msvc128_t, patomic_opimpl_bit_test_compl_128_relaxed,
+        HIDE_P, patomic_RELAXED, do_cmpxchg_explicit_128,
+        do_get_bit_128, do_make_desired_compl_128
+    )
+#endif
+
+#endif  /* PATOMIC_IMPL_MSVC_HAS_IL_COMPARE_EXCHANGE_128 */
+
+
 #endif  /* defined(_MSC_VER) */
 
 #endif  /* PATOMIC_IMPL_MSVC_OPS_BIT_MODIFY_COMPL_H */
