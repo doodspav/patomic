3ddab5fd3d02be7cad9a90be425316f8f11b5c06
doodspav <doodspav@gmail.com>
2024-12-30 15:34:00 +0000

    GHI #47 Fix slash alignment

--- src/impl/msvc/ops/binary/or.h
+++ src/impl/msvc/ops/binary/or.h
@@ -17,45 +17,45 @@
 
 
 #define do_fetch_or_explicit_n(n, type, obj, arg, order, res) \
-    do {                                                       \
-        switch (order)                                         \
-        {                                                      \
-            case patomic_RELAXED:                              \
+    do {                                                      \
+        switch (order)                                        \
+        {                                                     \
+            case patomic_RELAXED:                             \
       VIS_IL_NF((res = _InterlockedOr##n##_nf(obj, arg));)    \
-      VIS_IL_NF(break;)                                        \
-            case patomic_CONSUME:                              \
-            case patomic_ACQUIRE:                              \
+      VIS_IL_NF(break;)                                       \
+            case patomic_CONSUME:                             \
+            case patomic_ACQUIRE:                             \
      VIS_IL_ACQ((res = _InterlockedOr##n##_acq(obj, arg));)   \
-     VIS_IL_ACQ(break;)                                        \
-            case patomic_RELEASE:                              \
+     VIS_IL_ACQ(break;)                                       \
+            case patomic_RELEASE:                             \
      VIS_IL_REL((res = _InterlockedOr##n##_rel(obj, arg));)   \
-     VIS_IL_REL(break;)                                        \
-            case patomic_ACQ_REL:                              \
-            case patomic_SEQ_CST:                              \
-            default:                                           \
+     VIS_IL_REL(break;)                                       \
+            case patomic_ACQ_REL:                             \
+            case patomic_SEQ_CST:                             \
+            default:                                          \
                 res = _InterlockedOr##n(obj, arg);            \
-        }                                                      \
-    }                                                          \
+        }                                                     \
+    }                                                         \
     while (0)
 
 #define do_void_or_explicit_n(n, type, obj, arg, order)        \
-    do {                                                        \
-        type res;                                               \
+    do {                                                       \
+        type res;                                              \
         do_fetch_or_explicit_n(n, type, obj, arg, order, res); \
-        PATOMIC_IGNORE_UNUSED(res);                             \
-    }                                                           \
+        PATOMIC_IGNORE_UNUSED(res);                            \
+    }                                                          \
     while (0)
 
-#define do_make_desired_or(type, exp, arg, des)                      \
+#define do_make_desired_or(type, exp, arg, des)                       \
     do {                                                              \
         des = (type) (((unsigned type) exp) | ((unsigned type) arg)); \
     }                                                                 \
     while (0)
 
 #define do_make_desired_or_128(type, exp, arg, des) \
-    do {                                             \
+    do {                                            \
         des = patomic_msvc128_or(exp, arg);         \
-    }                                                \
+    }                                               \
     while (0)
 
 
