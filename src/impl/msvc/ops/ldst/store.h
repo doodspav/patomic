69142d499cbf9f7f08e84933ae17850bbf46eb4c
doodspav <doodspav@gmail.com>
2024-12-30 22:39:30 +0000

    GHI #47 Setup load and store

---
+++ src/impl/msvc/ops/ldst/store.h
@@ -0,0 +1,21 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_LDST_STORE_H
+#define PATOMIC_IMPL_MSVC_OPS_LDST_STORE_H
+
+#ifdef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/wrapped/direct.h>
+
+#include <patomic/api/ops.h>
+
+
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_LDST_STORE_H */
