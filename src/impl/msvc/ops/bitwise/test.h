04a651547d9ed9f44e23a2e59ae419219c6c9f04
doodspav <doodspav@gmail.com>
2024-12-31 04:57:11 +0000

    GHI #47 Setup test

---
+++ src/impl/msvc/ops/bitwise/test.h
@@ -0,0 +1,20 @@
+/* Copyright (c) doodspav. */
+/* SPDX-License-Identifier: LGPL-3.0-or-later WITH LGPL-3.0-linking-exception */
+
+#ifndef PATOMIC_IMPL_MSVC_OPS_BITWISE_TEST_H
+#define PATOMIC_IMPL_MSVC_OPS_BITWISE_TEST_H
+
+#ifdef _MSC_VER
+
+#include "../base.h"
+
+#include <patomic/wrapped/direct.h>
+
+#include <patomic/api/ops.h>
+
+
+
+
+#endif  /* defined(_MSC_VER) */
+
+#endif  /* PATOMIC_IMPL_MSVC_OPS_BITWISE_TEST_H */
